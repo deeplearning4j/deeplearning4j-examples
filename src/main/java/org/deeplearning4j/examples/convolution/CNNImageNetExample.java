c508cdbf1444c9f8138d64dd5c56709e5a28396f
nyghtowl <warrick.melanie@gmail.com>
2015-11-10 11:44:05 -0800

    Revamped imagenet example to easily sample and limit numer of examples from multiple categories. Also made it easier to pass in different models.

--- src/main/java/org/deeplearning4j/examples/convolution/CNNImageNetExample.java
+++ src/main/java/org/deeplearning4j/examples/convolution/CNNImageNetExample.java
@@ -1,8 +1,10 @@
 package org.deeplearning4j.examples.convolution;
 
+import com.google.common.io.LimitInputStream;
 import org.apache.commons.io.FileUtils;
 import org.canova.api.records.reader.RecordReader;
 import org.canova.api.split.FileSplit;
+import org.canova.api.split.LimitFileSplit;
 import org.canova.image.recordreader.ImageNetRecordReader;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.AsyncDataSetIterator;
@@ -29,6 +31,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
@@ -47,57 +50,34 @@ public class CNNImageNetExample {
 
     public static void main(String[] args) throws Exception {
         // libraries like Caffe scale to 256?
-        final int numRows = 224;  // TODO should be 224 based on VGG and AlexNet original paper
+        final int numRows = 224;
         final int numColumns = 224;
         int nChannels = 3;
         int outputNum = 1860;
-        int numBatches = 1; // TODO - total training amount for CSL is 1281167
+        int numBatches = 5; // TODO - total training amount for CSL is 1281167
         int batchSize = 20;
-        int iterations = 5;
-        int nTrainEpochs = 1;
+        int iterations = 1;
+        int numTrainEpochs = 5;
         int seed = 123;
         int listenerFreq = 1;
-        int splitTrainNum = (int) (batchSize * .9);
         MultiLayerNetwork model = null;
-        String modelType = "LeNet";
+        String modelType = "AlexNet";
         boolean gradientCheck = false;
         boolean train = true;
         DataSetIterator dataIter;
-        AsyncDataSetIterator asyncIter;
+        int totalNumExamples = batchSize*numBatches*numTrainEpochs;
+        int splitTrainNum = (int) (batchSize * .8);
+        int numTestExamples = totalNumExamples/(numBatches*numTrainEpochs) - splitTrainNum;
 
         String basePath = System.getProperty("user.home") + File.separator + "Documents" + File.separator + "skymind" + File.separator + "imagenet" + File.separator;
         String dataPath = basePath + "sample-pics" + File.separator;
         String labelPath = basePath + "cls-loc-labels.csv";
-
-        List<File> someFiles = new ArrayList<>();
-        Random rnd = new Random();
-        rnd.setSeed(seed);
-        boolean val;
-
-        File fileBase = new File(dataPath);
-        final List<String> allForm = Arrays.asList("jpg", "jpeg", "JPG", "JPEG");
-        File[] paths = fileBase.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                if (pathname.toString().endsWith("jpeg") || pathname.toString().endsWith("JPEG")) {
-                    return true;
-                }
-                return false;
-            }
-        });
-// TODO need to recursively loop through dir - haven't found a method yet that does this
-        for(File p: paths) {
-            System.out.println(p.getName());
-            if(rnd.nextBoolean())
-                someFiles.add(new File(p.getPath()));
-        }
-
+        String[] allForms = {"jpg", "jpeg", "JPG", "JPEG"};
 
 
         log.info("Load data....");
         RecordReader recordReader = new ImageNetRecordReader(numColumns, numRows, nChannels, true, labelPath);
-//        recordReader.initialize(new FileSplit(new File(dataPath)));
-        recordReader.initialize(dataPath, seed, numBatches*splitTrainNum);
+        recordReader.initialize(new LimitFileSplit(new File(dataPath), allForms, totalNumExamples, 2, Pattern.quote("_"), 0, new Random(123)));
         dataIter = new RecordReaderDataSetIterator(recordReader, batchSize, numRows * numColumns * nChannels, 1860);
         Evaluation eval = new Evaluation(recordReader.getLabels());
 
@@ -118,52 +98,41 @@ public class CNNImageNetExample {
         model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq)));
 
         if (gradientCheck) gradientCheck(dataIter, model);
-
-
+        
         if (train) {
             log.info("Train model....");
 
-            for (int i = 0; i < nTrainEpochs; i++) {
+            for (int i = 0; i < numTrainEpochs; i++) {
                 //TODO need dataIter that loops through set number of examples like SamplingIter but takes iter vs dataset
                 dataIter = new RecordReaderDataSetIterator(recordReader, batchSize, numRows * numColumns * nChannels, 1860);
 //                asyncIter = new AsyncDataSetIterator(dataIter, 1); TODO doesn't have next(num)
                 for (int j = 0; j < numBatches; j++)
-                    model.fit(dataIter.next(batchSize*splitTrainNum));
+                    model.fit(dataIter.next(splitTrainNum));
                 System.out.println("Epoch " + i + " complete");
 
                 //Evaluate classification performance:
-                eval = evaluatePerformance(model, dataIter, batchSize*(1-splitTrainNum), numBatches, eval);
+                eval = evaluatePerformance(model, dataIter, numTestExamples, numBatches, eval);
             }
-
-            //        SplitTestAndTrain trainTest;
-//        DataSet trainInput;
-//        List<INDArray> testInput = new ArrayList<>();
-//        List<INDArray> testLabels = new ArrayList<>();
-//        Map<Integer, String> testLabelMap = new LinkedHashMap<>();
-
-            // Small sample run - can't scale too far
-//            for(int i = 0; i < numBatches; i++) {
-//                imgNet = dataIter.next();
-//                imgNet.normalizeZeroMeanZeroUnitVariance();
-//                trainTest = imgNet.splitTestAndTrain(splitTrainNum, new Random(seed)); // train set that is the result
-//                trainInput = trainTest.getTrain(); // get feature matrix and labels for training
-//                testInput.add(trainTest.getTest().getFeatureMatrix());
-//                testLabels.add(trainTest.getTest().getLabels());
-//                model.fit(trainInput);
-//            }
-            // Small test setup
-//            Evaluation eval = new Evaluation(dataIter.labelMap());
-//            for(int i = 0; i < testInput.size(); i++) {
-//                INDArray output = model.output(testInput.get(i));
-//                eval.eval(testLabels.get(i), output);
-//            }
-
             log.info(eval.stats());
 
             log.info("****************Example finished********************");
         }
     }
 
+
+    private static Evaluation evaluatePerformance(MultiLayerNetwork model, DataSetIterator iter, int numExamples, int numBatches, Evaluation eval){
+        log.info("Evaluate model....");
+        DataSet imgNet;
+        INDArray output;
+
+        //TODO setup iterator to randomize and split test and train
+        for(int i=0; i < numBatches; i++){
+            imgNet = iter.next(numExamples);
+            output = model.output(imgNet.getFeatureMatrix());
+            eval.eval(imgNet.getLabels(), output);
+        }
+        return eval;
+    }
 
     private static void gradientCheck(DataSetIterator dataIter, MultiLayerNetwork model){
         DataSet imgNet;
@@ -199,18 +168,5 @@ public class CNNImageNetExample {
 
     }
 
-    private static Evaluation evaluatePerformance(MultiLayerNetwork model, DataSetIterator iter, int numExamples, int numBatches, Evaluation eval){
-        log.info("Evaluate model....");
-        DataSet imgNet;
-        INDArray output;
-
-        //TODO setup iterator to randomize and split test and train
-        for(int i=0; i < numBatches; i++){
-            imgNet = iter.next();
-            output = model.output(imgNet.getFeatureMatrix());
-            eval.eval(imgNet.getLabels(), output);
-        }
-        return eval;
-    }
 
 }
