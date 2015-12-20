18bc214915a9d37a6c9dd107e5e7656595b16ba2
nyghtowl <warrick.melanie@gmail.com>
2015-12-19 17:38:11 -0800

    Revised lfw example. It learns and returns f1 now

--- src/main/java/org/deeplearning4j/examples/convolution/CNNLFWExample.java
+++ src/main/java/org/deeplearning4j/examples/convolution/CNNLFWExample.java
@@ -1,6 +1,6 @@
 package org.deeplearning4j.examples.convolution;
 
-import org.deeplearning4j.datasets.fetchers.LFWDataFetcher;
+import org.canova.image.loader.LFWLoader;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.datasets.iterator.impl.LFWDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
@@ -8,12 +8,12 @@ import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
 import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
-import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
-import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToCnnPreProcessor;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
@@ -31,24 +31,31 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * @author Adam Gibson
+ * Reference: architecture partially based on DeepFace: http://mmlab.ie.cuhk.edu.hk/pdf/YiSun_CVPR14.pdf
+ * Note: this is a sparse dataset with only 1 example for many of the faces; thus, performance is low.
+ * Ideally train on a larger dataset like celebs to get params.
+ *
+ * Currently set to only use the subset images, names starting with A.
+ * Switch to NUM_LABELS & NUM_IMAGES to use full dataset.
  */
+
 public class CNNLFWExample {
     private static final Logger log = LoggerFactory.getLogger(CNNMnistExample.class);
 
     public static void main(String[] args) {
 
 
-        final int numRows = 28;
-        final int numColumns = 28;
-        int nChannels = 1;
-        int outputNum = 5749;
-        int numSamples = 2000;
-        int batchSize = 500;
-        int iterations = 10;
+        final int numRows = 40;
+        final int numColumns = 40;
+        int nChannels = 3;
+        int outputNum = LFWLoader.SUB_NUM_LABELS;
+        int numSamples = LFWLoader.SUB_NUM_IMAGES-4;
+        int batchSize = numSamples/10;
+        int iterations = 5;
         int splitTrainNum = (int) (batchSize*.8);
         int seed = 123;
         int listenerFreq = iterations/5;
+        boolean useSubset = true;
         DataSet lfwNext;
         SplitTestAndTrain trainTest;
         DataSet trainInput;
@@ -57,26 +64,59 @@ public class CNNLFWExample {
 
 
         log.info("Load data....");
-        DataSetIterator lfw = new LFWDataSetIterator(batchSize, numSamples);
+        DataSetIterator lfw = new LFWDataSetIterator(batchSize, numSamples, new int[] {numRows, numColumns, nChannels}, outputNum, useSubset, new Random(seed));
 
         log.info("Build model....");
         MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
                 .seed(seed)
                 .iterations(iterations)
+                .activation("relu")
+                .weightInit(WeightInit.XAVIER)
                 .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .list(3)
-                .layer(0, new ConvolutionLayer.Builder(10, 10)
+                .learningRate(0.01)
+                .momentum(0.9)
+                .regularization(true)
+                .updater(Updater.ADAGRAD)
+                .useDropConnect(true)
+                .list(6)
+                .layer(0, new ConvolutionLayer.Builder(4, 4)
+                        .name("cnn1")
                         .nIn(nChannels)
-                        .nOut(6)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation("relu")
+                        .stride(1, 1)
+                        .nOut(20)
                         .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[] {2,2})
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                        .name("pool1")
                         .build())
-                .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .layer(2, new ConvolutionLayer.Builder(3, 3)
+                        .name("cnn2")
+                        .stride(1,1)
+                        .nOut(40)
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                        .name("pool2")
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(3, 3)
+                        .name("cnn3")
+                        .stride(1,1)
+                        .nOut(60)
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                        .name("pool3")
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(2, 2)
+                        .name("cnn3")
+                        .stride(1,1)
+                        .nOut(80)
+                        .build())
+                .layer(4, new DenseLayer.Builder()
+                        .name("ffn1")
+                        .nOut(160)
+                        .dropOut(0.5)
+                        .build())
+                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
                         .nOut(outputNum)
-                        .weightInit(WeightInit.XAVIER)
                         .activation("softmax")
                         .build())
                 .backprop(true).pretrain(false);
@@ -99,7 +139,7 @@ public class CNNLFWExample {
         }
 
         log.info("Evaluate model....");
-        Evaluation eval = new Evaluation(outputNum);
+        Evaluation eval = new Evaluation(lfw.getLabels());
         for(int i = 0; i < testInput.size(); i++) {
             INDArray output = model.output(testInput.get(i));
             eval.eval(testLabels.get(i), output);
