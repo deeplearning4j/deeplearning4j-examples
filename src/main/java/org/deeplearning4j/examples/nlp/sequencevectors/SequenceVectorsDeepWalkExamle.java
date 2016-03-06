476ad7ceed6a1bc341f6593ff055bcad517808b0
raver119 <raver119@gmail.com>
2016-03-06 13:55:23 +0300

    initial commit

---
+++ src/main/java/org/deeplearning4j/examples/nlp/sequencevectors/SequenceVectorsDeepWalkExamle.java
@@ -0,0 +1,16 @@
+package org.deeplearning4j.examples.nlp.sequencevectors;
+
+/**
+ *
+ * * *************************************************************************************************
+ * PLEASE NOTE: THIS EXAMPLE REQUIRES DL4J/ND4J VERSIONS >= rc3.9 TO COMPILE SUCCESSFULLY
+ * *************************************************************************************************
+ *
+ * @author raver119@gmail.com
+ */
+public class SequenceVectorsDeepWalkExamle {
+
+    public static void main(String[] args) {
+
+    }
+}
