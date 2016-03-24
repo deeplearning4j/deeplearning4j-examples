1bfa60b39072cdbc2bce0d3a7238aa59f7e1b0d9
Alex Black <blacka101@gmail.com>
2016-03-24 22:00:03 +1100

    Add more comments to CSV example explaining values

--- src/main/java/org/deeplearning4j/examples/misc/csv/CSVExample.java
+++ src/main/java/org/deeplearning4j/examples/misc/csv/CSVExample.java
@@ -37,8 +37,8 @@ public class CSVExample {
         recordReader.initialize(new FileSplit(new ClassPathResource("iris.txt").getFile()));
 
         //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
-        int labelIndex = 4;
-        int numClasses = 3;
+        int labelIndex = 4;     //5 values in each row of the iris.txt CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
+        int numClasses = 3;     //3 classes (types of iris flowers) in the iris data set. Classes have integer values 0, 1 or 2
         int batchSize = 150;    //Iris data set: 150 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
         DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader,batchSize,labelIndex,numClasses);
 
