80af90d4126026b9ec01c54f55f2c61416f8af59
Atilla Ozgur <ati.ozgur@gmail.com>
2016-05-22 17:32:44 +0300

    default goal ok

---
+++ src/main/java/org/deeplearning4j/examples/RunAllExamples.java
@@ -0,0 +1,18 @@
+package org.deeplearning4j.examples;
+
+
+/**
+ * @author Atilla Ozgur
+ */
+public class RunAllExamples {
+
+    private static String[] exampleList = null;
+
+    public static void main(String[] args) throws  Exception {
+
+        System.out.println("Hello Deep Learning4J");
+
+    }
+
+
+}
