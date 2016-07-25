9aa151dfc4e973a15c5c3dbf86ba008856789290
Alex Black <blacka101@gmail.com>
2016-07-25 23:04:04 +1000

    Update normalization in CSV example'

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/dataExamples/CSVExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/dataExamples/CSVExample.java
@@ -17,6 +17,8 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,10 +42,21 @@ public class CSVExample {
         int labelIndex = 4;     //5 values in each row of the iris.txt CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
         int numClasses = 3;     //3 classes (types of iris flowers) in the iris data set. Classes have integer values 0, 1 or 2
         int batchSize = 150;    //Iris data set: 150 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
+
         DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader,batchSize,labelIndex,numClasses);
+        DataSet allData = iterator.next();
+        allData.shuffle();
+        SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);  //Use 65% of data for training
 
+        DataSet trainingData = testAndTrain.getTrain();
+        DataSet testData = testAndTrain.getTest();
 
-        DataSet next = iterator.next();
+        //We need to normalize our data. We'll use NormalizeStandardize (which gives us mean 0, unit variance):
+        DataNormalization normalizer = new NormalizerStandardize();
+        normalizer.fit(trainingData);           //Collect the statistics (mean/stdev) from the training data. This does not modify the input data
+        normalizer.transform(trainingData);     //Apply normalization to the training data
+        normalizer.transform(testData);         //Apply normalization to the test data. This is using statistics calculated from the *training* set
+
 
         final int numInputs = 4;
         int outputNum = 3;
@@ -75,20 +88,12 @@ public class CSVExample {
         model.init();
         model.setListeners(new ScoreIterationListener(100));
 
-        //Normalize the full data set. Our DataSet 'next' contains the full 150 examples
-        next.normalizeZeroMeanZeroUnitVariance();
-        next.shuffle();
-        //split test and train
-        SplitTestAndTrain testAndTrain = next.splitTestAndTrain(0.65);  //Use 65% of data for training
-
-        DataSet trainingData = testAndTrain.getTrain();
         model.fit(trainingData);
 
         //evaluate the model on the test set
         Evaluation eval = new Evaluation(3);
-        DataSet test = testAndTrain.getTest();
-        INDArray output = model.output(test.getFeatureMatrix());
-        eval.eval(test.getLabels(), output);
+        INDArray output = model.output(testData.getFeatureMatrix());
+        eval.eval(testData.getLabels(), output);
         log.info(eval.stats());
     }
 
