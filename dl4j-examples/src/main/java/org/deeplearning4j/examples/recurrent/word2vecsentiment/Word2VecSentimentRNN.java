2835828acd4ec2b577c954c9dc192977e6b02431
raver119 <raver119@gmail.com>
2016-10-11 18:46:30 +0300

    use staticWord2Vec in sentimentRNN example, to make it more suitable on GPU systems

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/Word2VecSentimentRNN.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/Word2VecSentimentRNN.java
@@ -87,7 +87,8 @@ public class Word2VecSentimentRNN {
 
         //DataSetIterators for training and testing respectively
         //Using AsyncDataSetIterator to do data loading in a separate thread; this may improve performance vs. waiting for data to load
-        WordVectors wordVectors = WordVectorSerializer.loadGoogleModel(new File(WORD_VECTORS_PATH), true, false);
+        // PLEASE NOTE: we're using loadStaticModel method here, which DOESN'T load Word2Vec model into GPU, and passes vectors there one by one
+        WordVectors wordVectors = WordVectorSerializer.loadStaticModel(new File(WORD_VECTORS_PATH));
         DataSetIterator train = new AsyncDataSetIterator(new SentimentExampleIterator(DATA_PATH,wordVectors,batchSize,truncateReviewsToLength,true),1);
         DataSetIterator test = new AsyncDataSetIterator(new SentimentExampleIterator(DATA_PATH,wordVectors,100,truncateReviewsToLength,false),1);
 
