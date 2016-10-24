ce2d6529ffc48db845a941b6f796765fda70bf70
wieland959 <raphael.wieland@gmail.com>
2016-10-24 15:37:50 -0400

    Finished updating inline documentation.

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/mnist/MLPMnistTwoLayerExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/mnist/MLPMnistTwoLayerExample.java
@@ -20,7 +20,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-/** A MLP applied to digit classification for MNIST. */
+/** A slightly more involved multilayered (MLP) applied to digit classification for the MNIST dataset (http://yann.lecun.com/exdb/mnist/). 
+*
+* This example uses two input layers and one hidden layer.
+* 
+* The first input layer has input dimension of numRows*numColumns where these variables indicate the
+* number of vertical and horizontal pixels in the image. This layer uses a rectified linear unit
+* (relu) activation function. The weights for this layer are initialized by using Xavier initialization
+* (https://prateekvjoshi.com/2016/03/29/understanding-xavier-initialization-in-deep-neural-networks/)
+* to avoid having a steep learning curve. This layer sends 500 output signals to the second layer.
+*
+* The second input layer has input dimension of 500. This layer also uses a rectified linear unit
+* (relu) activation function. The weights for this layer are also initialized by using Xavier initialization
+* (https://prateekvjoshi.com/2016/03/29/understanding-xavier-initialization-in-deep-neural-networks/)
+* to avoid having a steep learning curve. This layer sends 100 output signals to the hidden layer.
+*
+* The hidden layer has input dimensions of 100. These are fed from the second input layer. The weights
+* for this layer is also initialized using Xavier initialization. The activation function for this
+* layer is a softmax, which normalizes all the 10 outputs such that the normalized sums
+* add up to 1. The highest of these normalized values is picked as the predicted class.
+* 
+*/
 public class MLPMnistTwoLayerExample {
 
     private static Logger log = LoggerFactory.getLogger(MLPMnistSingleLayerExample.class);
@@ -33,7 +53,7 @@ public class MLPMnistTwoLayerExample {
         int batchSize = 64; // batch size for each epoch
         int rngSeed = 123; // random number seed for reproducibility
         int numEpochs = 15; // number of epochs to perform
-        double rate = 0.0015;
+        double rate = 0.0015; // learning rate
 
         //Get the DataSetIterators:
         DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
@@ -49,14 +69,13 @@ public class MLPMnistTwoLayerExample {
             .weightInit(WeightInit.XAVIER)
             .learningRate(rate) //specify the learning rate
             .updater(Updater.NESTEROVS).momentum(0.98) //specify the rate of change of the learning rate.
-            .regularization(true).l2(rate * 0.005)
+            .regularization(true).l2(rate * 0.005) // regularize learning model
             .list()
             .layer(0, new DenseLayer.Builder() //create the first input layer.
                     .nIn(numRows * numColumns)
                     .nOut(500)
                     .build())
-            .layer(1,  new DenseLayer.Builder() //create the second input layer
-
+            .layer(1, new DenseLayer.Builder() //create the second input layer
                     .nIn(500)
                     .nOut(100)
                     .build())
@@ -70,7 +89,7 @@ public class MLPMnistTwoLayerExample {
 
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
-        model.setListeners(new ScoreIterationListener(5));  //print the score with every 1 iteration
+        model.setListeners(new ScoreIterationListener(5));  //print the score with every iteration
 
         log.info("Train model....");
         for( int i=0; i<numEpochs; i++ ){
