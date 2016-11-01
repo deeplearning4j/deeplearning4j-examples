c8a3443539a6cdfec1f8a78ad9c80da6305035f7
24man <timrutledge12@gmail.com>
2016-11-01 16:07:58 -0400

    Added Documentation to Word2VecRawTextExample.java
    Also, Changed one System.out.print line to include a bit of information about what it is printing.

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/Word2VecRawTextExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/Word2VecRawTextExample.java
@@ -16,13 +16,17 @@ import java.util.Collection;
 
 /**
  * Created by agibsonccc on 10/9/14.
+ *
+ * Neural net that processes text into wordvectors. See below url for an in-depth explanation.
+ * https://deeplearning4j.org/word2vec.html
  */
 public class Word2VecRawTextExample {
 
     private static Logger log = LoggerFactory.getLogger(Word2VecRawTextExample.class);
 
     public static void main(String[] args) throws Exception {
-
+        
+        // Gets Path to Text file
         String filePath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
 
         log.info("Load & Vectorize Sentences....");
@@ -48,12 +52,14 @@ public class Word2VecRawTextExample {
 
         log.info("Writing word vectors to text file....");
 
-        // Write word vectors
+        // Write word vectors to file
         WordVectorSerializer.writeWordVectors(vec, "pathToWriteto.txt");
 
+        // Prints out the closest 10 words to "day". An example on what to do with these Word Vectors.
         log.info("Closest Words:");
         Collection<String> lst = vec.wordsNearest("day", 10);
-        System.out.println(lst);
+        System.out.println("10 Words closest to 'day': " + lst);
+        
         UiServer server = UiServer.getInstance();
         System.out.println("Started on port " + server.getPort());
     }
