04ba58a8afe608aa8f1b53e7399b46f3c4c155ba
eraly <susan.eraly@gmail.com>
2016-12-28 15:21:31 -0800

    from numpy

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/misc/modelsaving/KerasModelImport.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/misc/modelsaving/KerasModelImport.java
@@ -1,76 +1,110 @@
 package org.deeplearning4j.examples.misc.modelsaving;
 
 /**
- * Created by susaneraly on 12/20/16.
+ * @author eraly
  */
 
-import org.datavec.api.io.filters.BalancedPathFilter;
-import org.datavec.api.io.labels.ParentPathLabelGenerator;
-import org.datavec.api.split.FileSplit;
-import org.datavec.image.loader.BaseImageLoader;
-import org.datavec.image.recordreader.ImageRecordReader;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.Model;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastSubOp;
-import org.nd4j.linalg.cpu.nativecpu.NDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
-import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.factory.Nd4j;
-import org.datavec.api.split.InputSplit;
-import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.linalg.indexing.BooleanIndexing;
+import org.nd4j.linalg.indexing.conditions.Conditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.Random;
 
 
 public class KerasModelImport {
 
     protected static Logger logger= LoggerFactory.getLogger(KerasModelImport.class);
 
-    public static final String TRAIN_DIR = "/Users/susaneraly/SKYMIND/kerasImport/blogPost/data/train";
+    //public static final String TRAIN_DIR = "/Users/susaneraly/SKYMIND/kerasImport/blogPost/data/train";
     public static final String MODEL_DIR = "/Users/susaneraly/SKYMIND/kerasImport/VGG16/saved";
+    public static final String NUMPY_DIR = "/Users/susaneraly/SKYMIND/kerasImport/VGG16/deep-learning-models/validation/dogs";
+    public static final String FILE_PREFIX = "/dog";
+    public static final int MIN_INDEX = 0;
+    public static final int MAX_INDEX = 399;
+    public static final String INPUT_FILE_SUFFIX = ".csv";
+    public static final String OUTPUT_FILE_SUFFIX = "Out.csv";
 
-    protected static final String [] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS;
-    protected static final long seed = 12345;
-    public static final Random rng = new Random(seed);
-    protected static int height = 224;
-    protected static int width = 224;
-    protected static int channels = 3;
-    public static int numExamples = 1000;
-    public static int batchSize = 1;
-    public static int numLabels = 2;
+    public static int batchSize = 10;
 
-    public static final INDArray VGG_MEAN_OFFSET = Nd4j.create(new double[] {103.939,116.779,123.68});
+    //public static final INDArray VGG_MEAN_OFFSET = Nd4j.create(new double[] {103.939,116.779,123.68});
 
 
     public static void main(String[] args) throws Exception{
 
-        logger.info("Loading VGG...");
-        //ComputationGraph vggNet= Model.importModel(MODEL_DIR + "/vgg16.json", MODEL_DIR + "/vgg16.h5");
-        ComputationGraph vggNet = Model.importFunctionalApiModel(MODEL_DIR + "/vggmodel.h5");
+        loadFromNumPy loader = new loadFromNumPy(batchSize);
 
+        logger.info("Loading VGG...");
+        ComputationGraph vggNet= Model.importFunctionalApiModel(MODEL_DIR + "/vgg16.json", MODEL_DIR + "/vgg16.h5");
+
+        int incorrect = 0;
+        int batchCount = 0;
+        while (loader.hasNext()) {
+            DataSet imageSet = loader.next();
+            INDArray features = imageSet.getFeatures();
+            INDArray labels = imageSet.getLabels();
+            INDArray[] outputA = vggNet.output(false,features);
+            INDArray output = Nd4j.concat(0,outputA);
+
+            INDArray kerasClass = Nd4j.argMax(labels,1);
+            INDArray dl4jClass = Nd4j.argMax(output,1);
+            INDArray comp = kerasClass.sub(dl4jClass);
+            BooleanIndexing.replaceWhere(comp,1, Conditions.notEquals(0));
+            if (kerasClass != dl4jClass) incorrect+= comp.sumNumber().intValue();
+            System.out.println("Keras argmax:\n"+ kerasClass);
+            System.out.println("DL4J argmax:\n" + dl4jClass);
+
+            System.out.println(output.sub(labels).max(1));
+
+            batchCount++;
+            System.out.println("==============");
+            if (batchCount == 10) break;
+        }
+        System.out.println(incorrect+" predictions different from keras");
+
+        /*
         logger.info("Loading images and building the iterator...");
         DataSetIterator imageIterator = getImageIterator();
+        */
 
-        while (imageIterator.hasNext()) {
-            DataSet next = imageIterator.next(1000);
-            INDArray labels = next.getLabels();
-            System.out.println(Arrays.toString(labels.shape()));
-            INDArray features = next.getFeatures();
-            System.out.println(Arrays.toString(features.shape()));
-            //INDArray predictions = vggNet.output(false,features[0]);
-            //logger.info(eval.stats(true));
+    }
+
+    public static class loadFromNumPy {
+
+        private int currentIndex = MIN_INDEX;
+        private int batchSize = 1;
+
+        public loadFromNumPy(int batchSize) {
+            this.batchSize = batchSize;
+        }
+
+        public DataSet next() throws IOException {
+            INDArray features = Nd4j.readNumpy(NUMPY_DIR + FILE_PREFIX + currentIndex + INPUT_FILE_SUFFIX, " ").reshape(1, 3, 224, 224);
+            INDArray labels = Nd4j.readNumpy(NUMPY_DIR + FILE_PREFIX + currentIndex + OUTPUT_FILE_SUFFIX, " ").reshape(1, 1000);
+            for (int i=currentIndex+1; i<currentIndex+batchSize;i++) {
+                INDArray f = Nd4j.readNumpy(NUMPY_DIR + FILE_PREFIX + i + INPUT_FILE_SUFFIX, " ").reshape(1, 3, 224, 224);
+                INDArray l = Nd4j.readNumpy(NUMPY_DIR + FILE_PREFIX + i + OUTPUT_FILE_SUFFIX, " ").reshape(1, 1000);
+                features = Nd4j.concat(0,features,f);
+                labels = Nd4j.concat(0,labels,l);
+            }
+            currentIndex = currentIndex+batchSize;
+            return new org.nd4j.linalg.dataset.DataSet(features,labels);
+        }
+
+        public boolean hasNext() {
+            return currentIndex+batchSize-1 < MAX_INDEX;
         }
 
     }
 
+    /*
+    //Currently unused
     public static DataSetIterator getImageIterator() throws Exception{
 
         File mainPath = new File(TRAIN_DIR);
@@ -91,12 +125,13 @@ public class KerasModelImport {
         return dataIter;
     }
 
+    //Currently unused
     protected static class processForVGG implements DataSetPreProcessor {
-        /*
-                resized_image[:,:,0] -= 103.939
-                resized_image[:,:,1] -= 116.779
-                resized_image[:,:,2] -= 123.68
-         */
+        //
+        //      resized_image[:,:,0] -= 103.939
+        //      resized_image[:,:,1] -= 116.779
+        //      resized_image[:,:,2] -= 123.68
+        //
 
         @Override
         public void preProcess(DataSet toPreProcess) {
@@ -105,4 +140,6 @@ public class KerasModelImport {
         }
 
     }
+    */
+
 }
