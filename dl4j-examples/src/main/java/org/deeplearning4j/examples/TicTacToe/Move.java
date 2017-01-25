7d326a2fdc0927a0160b90874bdc18aa6a79cdaf
kitsolDeepLearning <trupesh@kitsol.in>
2017-01-25 20:24:34 +0530

    Updated source code based on Adam's comment. Code for following classe was compeletly rewritten. 1)TicTacToePlayer 2)PlayTicTacToe 3)TicTacToeGameTrainer 4)TicTacToeData

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/TicTacToe/Move.java
+++
@@ -1,9 +0,0 @@
-package org.deeplearning4j.examples.TicTacToe;
-
-/**
- * Developed by KIT Solutions Pvt. Ltd.( www.kitsol.com) on 23-Aug-16.
- */
-public class Move {
-    int index;
-    double probability;
-}
