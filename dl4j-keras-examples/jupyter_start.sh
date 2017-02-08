e7fea032f4fe9547e07f4f9852f358e0c49bd2ca
Justin Long <justinlong@outlook.com>
2017-02-08 15:09:57 -0800

    Combined stdout for both Jupyter and DL4J gateway server.

--- dl4j-keras-examples/jupyter_start.sh
+++ dl4j-keras-examples/jupyter_start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo "Starting DL4J entrypoint server..."
-export LIBND4J_HOME=/libnd4j
-java -cp /keras-dl4j/target/dl4j-keras-examples.jar org.deeplearning4j.keras.Server >& /server.log &
-echo "Starting Jupyter..."
-jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/keras-dl4j/examples
+echo "Starting DL4J server and Jupyter..."
+{ java -cp /keras-dl4j/target/dl4j-keras-examples.jar org.deeplearning4j.keras.Server & jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/keras-dl4j/examples; }
