a9ac51903b723e8b502e8d0d40ba958a37c3701d
Justin Long <justinlong@outlook.com>
2017-02-08 00:32:28 -0800

    Fix Docker implementation so that it builds, runs, and properly packages libnd4j.

--- dl4j-keras-examples/jupyter_start.sh
+++ dl4j-keras-examples/jupyter_start.sh
@@ -2,6 +2,6 @@
 
 echo "Starting DL4J entrypoint server..."
 export LIBND4J_HOME=/libnd4j
-java -cp /deeplearning4j-examples/deeplearning4j-keras-examples/target/deeplearning4j-keras-examples-0.7.3-SNAPSHOT.jar org.deeplearning4j.keras.Server >& /server.log &
+java -cp /keras-dl4j/target/dl4j-keras-examples.jar org.deeplearning4j.keras.Server >& /server.log &
 echo "Starting Jupyter..."
 jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/keras-dl4j/examples
