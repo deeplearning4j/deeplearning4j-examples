cb99b162a1ee79b4d12fd7096d868061ddf5af98
Justin Long <justinlong@outlook.com>
2017-02-18 19:27:52 -0800

    Programatically set Xmx for JVM.

--- dl4j-keras-examples/jupyter_start.sh
+++ dl4j-keras-examples/jupyter_start.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Starting DL4J server and Jupyter..."
-{ java -cp /keras-dl4j/target/dl4j-keras-examples.jar -Dorg.bytedeco.javacpp.nopointergc=true org.deeplearning4j.keras.Server & jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/keras-dl4j/examples; }
+phymem=$(free -g|awk '/^Mem:/{print $2}')
+echo "There is $phymem available for java"
+{ java -Xmx${phymem}g -cp /keras-dl4j/target/dl4j-keras-examples.jar -Dorg.bytedeco.javacpp.nopointergc=true org.deeplearning4j.keras.Server & jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/keras-dl4j/examples; }
