3ddc11f24233c66e6877480f1c18c1fffb4b838d
Don A. Smith <ThinkerFeeler@gmail.com>
2017-03-25 09:57:22 -0700

    Initial checkin of melodl4j package for learning melodies from Midi files using an LSTM neural network.

---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/character/melodl4j/MelodyModelingExample.java
@@ -0,0 +1,235 @@
+package org.deeplearning4j.examples.recurrent.character.melodl4j;
+
+import org.deeplearning4j.examples.recurrent.character.CharacterIterator;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.BackpropType;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.apache.commons.io.FileUtils;
+import java.net.URL;
+import java.io.File;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**GravesLSTM  Symbolic melody modelling example, based closely on GravesLSTMCharModellingExample.java.
+ *  See the README file in this directory for documentation.
+ *
+ * @author Alex Black, Donald A. Smith.
+ *
+ */
+public class MelodyModelingExample {
+    final static String inputSymbolicMelodiesFilename = "midi-melodies-bach.txt"; // Try also midi-melodies-pop.txt
+    final static String tmpDir = System.getProperty("java.io.tmpdir");  // Change this directory to, say, /tmp/ if that's more convenient for you.
+
+    final static String symbolicMelodiesInputFilePath = tmpDir + "/" + inputSymbolicMelodiesFilename;  // Point to melodies created by Midi2Symbolic.java
+    final static String composedMelodiesOutputFilePath = tmpDir + "/composition.txt"; // You can listen to these melodies by running PlaySymbolic.java against this file.
+    //....
+    public static void main( String[] args ) throws Exception {
+        int lstmLayerSize = 200;					//Number of units in each GravesLSTM layer
+        int miniBatchSize = 32;						//Size of mini batch to use when  training
+        int exampleLength = 500; //1000;		//Length of each training example sequence to use. This could certainly be increased
+        int tbpttLength = 50;                       //Length for truncated backpropagation through time. i.e., do parameter updates ever 50 characters
+        int numEpochs = 30;							//Total number of training epochs
+        int generateSamplesEveryNMinibatches = 20;  //How frequently to generate samples from the network? 1000 characters / 50 tbptt length: 20 parameter updates per minibatch
+        int nSamplesToGenerate = 10;					//Number of samples to generate after each training epoch
+        int nCharactersToSample = 300;				//Length of each sample to generate
+        String generationInitialization = null;		//Optional character initialization; a random character is used if null
+        // Above is Used to 'prime' the LSTM with a character sequence to continue/complete.
+        // Initialization characters must all be in CharacterIterator.getMinimalCharacterSet() by default
+        Random rng = new Random(12345);
+        long startTime = System.currentTimeMillis();
+
+        //Get a DataSetIterator that handles vectorization of text into something we can use to train
+        // our GravesLSTM network.
+        CharacterIterator iter = getMidiIterator(miniBatchSize,exampleLength);
+        int nOut = iter.totalOutcomes();
+
+        //Set up network configuration:
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
+            .learningRate(0.1)
+            .rmsDecay(0.95)
+            .seed(12345)
+            .regularization(true)
+            .l2(0.001)
+            .weightInit(WeightInit.XAVIER)
+            .updater(Updater.RMSPROP)
+            .list()
+            .layer(0, new GravesLSTM.Builder().nIn(iter.inputColumns()).nOut(lstmLayerSize)
+                .activation(Activation.TANH).build())
+            .layer(1, new GravesLSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
+                .activation(Activation.TANH).build())
+//            .layer(2, new GravesLSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
+//                .activation(Activation.TANH).build())
+            .layer(2, new RnnOutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX)        //MCXENT + softmax for classification
+                .nIn(lstmLayerSize).nOut(nOut).build())
+            .backpropType(BackpropType.TruncatedBPTT).tBPTTForwardLength(tbpttLength).tBPTTBackwardLength(tbpttLength)
+            .pretrain(false).backprop(true)
+            .build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+        net.setListeners(new ScoreIterationListener(100));
+
+        //Print the  number of parameters in the network (and for each layer)
+        Layer[] layers = net.getLayers();
+        int totalNumParams = 0;
+        for( int i=0; i<layers.length; i++ ){
+            int nParams = layers[i].numParams();
+            System.out.println("Number of parameters in layer " + i + ": " + nParams);
+            totalNumParams += nParams;
+        }
+        System.out.println("Total number of network parameters: " + totalNumParams);
+
+        List<String> melodies = new ArrayList<>(); // Later we print them out in reverse
+        // order, so that the best melodies are at the start of the file.
+        //Do training, and then generate and print samples from network
+        int miniBatchNumber = 0;
+        for( int epoch=0; epoch<numEpochs; epoch++ ){
+            System.out.println("Starting epoch " + epoch);
+            while(iter.hasNext()){
+                DataSet ds = iter.next();
+                net.fit(ds);
+                if(++miniBatchNumber % generateSamplesEveryNMinibatches == 0){
+                    System.out.println("---------- epoch " + epoch + " --------------");
+                    System.out.println("Completed " + miniBatchNumber + " minibatches of size " + miniBatchSize + "x" + exampleLength + " characters" );
+                    System.out.println("Sampling characters from network given initialization \"" + (generationInitialization == null ? "" : generationInitialization) + "\"");
+                    String[] samples = sampleCharactersFromNetwork(generationInitialization,net,iter,rng,nCharactersToSample,nSamplesToGenerate);
+                    for( int j=0; j<samples.length; j++ ){
+                        System.out.println("----- Sample " + j + " ----- of epoch " + epoch);
+                        System.out.println(samples[j]);
+                        melodies.add(samples[j]);
+                        System.out.println();
+                    }
+                }
+            }
+            iter.reset();	//Reset iterator for another epoch
+            if (melodies.size()>0) {
+                String melody=melodies.get(melodies.size()-1);
+                // Play 15 seconds of the most recent melody
+                System.out.println("\nFirst 10 seconds of " + melody);
+                PlaySymbolic.playMelody(melody,15);
+            }
+        }
+        // Write all melodies to the output file, in reverse order (so that the best melodies are at the start of the file).
+        PrintWriter printWriter = new PrintWriter(composedMelodiesOutputFilePath);
+        for(int i=melodies.size()-1;i>=0;i--) {
+            printWriter.println(melodies.get(i));
+        }
+        printWriter.close();
+        double seconds = 0.001*(System.currentTimeMillis()-startTime);
+
+        System.out.println("\n\nExample complete in " + seconds + " seconds");
+    }
+
+    /** Sets up and return a simple DataSetIterator that does vectorization based on the melody sample.
+     * @param miniBatchSize Number of text segments in each training mini-batch
+     * @param sequenceLength Number of characters in each text segment.
+     */
+    public static CharacterIterator getMidiIterator(int miniBatchSize, int sequenceLength) throws Exception{
+        File f = new File(symbolicMelodiesInputFilePath);
+        if( !f.exists() ){
+            FileUtils.copyURLToFile(new URL("http://truthsite.org/music/" +inputSymbolicMelodiesFilename ), f);
+            if (!f.exists()) {
+                throw new RuntimeException(f.getAbsolutePath() + " does not exist");
+            }
+            System.out.println("File downloaded to " + f.getAbsolutePath());
+        } else {
+            System.out.println("Using existing text file at " + f.getAbsolutePath());
+        }
+        char[] validCharacters ="0123456789abc!@#$%^&*(ABCDdefghijklmnopqrstuvwzyzEFGHIJKLMR".toCharArray(); //Which characters are allowed? Others will be removed
+        return new CharacterIterator(symbolicMelodiesInputFilePath, Charset.forName("UTF-8"),
+            miniBatchSize, sequenceLength, validCharacters, new Random(12345));
+    }
+
+    /** Generate a sample from the network, given an (optional, possibly null) initialization. Initialization
+     * can be used to 'prime' the RNN with a sequence you want to extend/continue.<br>
+     * Note that the initalization is used for all samples
+     * @param initialization String, may be null. If null, select a random character as initialization for all samples
+     * @param charactersToSample Number of characters to sample from network (excluding initialization)
+     * @param net MultiLayerNetwork with one or more GravesLSTM/RNN layers and a softmax output layer
+     * @param iter CharacterIterator. Used for going from indexes back to characters
+     */
+    private static String[] sampleCharactersFromNetwork(String initialization, MultiLayerNetwork net,
+                                                        CharacterIterator iter, Random rng, int charactersToSample, int numSamples ){
+        //Set up initialization. If no initialization: use a random character
+        if( initialization == null ){
+            initialization = String.valueOf(iter.getRandomCharacter());
+        }
+
+        //Create input for initialization
+        INDArray initializationInput = Nd4j.zeros(numSamples, iter.inputColumns(), initialization.length());
+        char[] init = initialization.toCharArray();
+        for( int i=0; i<init.length; i++ ){
+            int idx = iter.convertCharacterToIndex(init[i]);
+            for( int j=0; j<numSamples; j++ ){
+                initializationInput.putScalar(new int[]{j,idx,i}, 1.0f);
+            }
+        }
+
+        StringBuilder[] sb = new StringBuilder[numSamples];
+        for( int i=0; i<numSamples; i++ ) sb[i] = new StringBuilder(initialization);
+
+        //Sample from network (and feed samples back into input) one character at a time (for all samples)
+        //Sampling is done in parallel here
+        net.rnnClearPreviousState();
+        INDArray output = net.rnnTimeStep(initializationInput);
+        output = output.tensorAlongDimension(output.size(2)-1,1,0);	//Gets the last time step output
+
+        for( int i=0; i<charactersToSample; i++ ){
+            //Set up next input (single time step) by sampling from previous output
+            INDArray nextInput = Nd4j.zeros(numSamples,iter.inputColumns());
+            //Output is a probability distribution. Sample from this for each example we want to generate, and add it to the new input
+            for( int s=0; s<numSamples; s++ ){
+                double[] outputProbDistribution = new double[iter.totalOutcomes()];
+                for( int j=0; j<outputProbDistribution.length; j++ ) outputProbDistribution[j] = output.getDouble(s,j);
+                int sampledCharacterIdx = sampleFromDistribution(outputProbDistribution,rng);
+
+                nextInput.putScalar(new int[]{s,sampledCharacterIdx}, 1.0f);		//Prepare next time step input
+                sb[s].append(iter.convertIndexToCharacter(sampledCharacterIdx));	//Add sampled character to StringBuilder (human readable output)
+            }
+
+            output = net.rnnTimeStep(nextInput);	//Do one time step of forward pass
+        }
+
+        String[] out = new String[numSamples];
+        for( int i=0; i<numSamples; i++ ) out[i] = sb[i].toString();
+        return out;
+    }
+
+    /** Given a probability distribution over discrete classes, sample from the distribution
+     * and return the generated class index.
+     * @param distribution Probability distribution over classes. Must sum to 1.0
+     */
+    public static int sampleFromDistribution( double[] distribution, Random rng ){
+        double d = 0.0;
+        double sum = 0.0;
+        for( int t=0; t<10; t++ ) {
+            d = rng.nextDouble();
+            sum = 0.0;
+            for( int i=0; i<distribution.length; i++ ){
+                sum += distribution[i];
+                if( d <= sum ) return i;
+            }
+            //If we haven't found the right index yet, maybe the sum is slightly
+            //lower than 1 due to rounding error, so try again.
+        }
+        //Should be extremely unlikely to happen if distribution is a valid probability distribution
+        throw new IllegalArgumentException("Distribution is invalid? d="+d+", sum="+sum);
+    }
+}
---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/character/melodl4j/README.md
@@ -0,0 +1,62 @@
+
+MELODL4J is a small java package in dl4j-examples for extracting melodies from MIDI, feeding them to a LSTM neural network, and composing music using deep learning.
+
+You train the network on a set of sample melodies. The neural network learns to generate new melodies similar to the input melodies.
+
+During network training, it plays melodies at the end of each training epoch. "Deep humming". At the end of training, it outputs all melodies to an output file.
+
+There's a utility for listening to the generated melodies.
+
+The LSTM neural network is based on the GravesLSTMCharModellingExample class by Alex Black in deeplearning4j.
+
+The generated melodies are monophonic, meaning that only one note plays at a time: there are no chords or harmony.
+
+No claim is made that this work is state of the art for computer music generation.  But the generated melodies do sound pleasant and interesting.  I pursued the project mostly to familiarize myself with deep learning and and deeplearning4j.  I hope the example is educational and fun for others, as well as a foundation for doing more complex musical composition.
+
+See http://www.asimovinstitute.org/analyzing-deep-learning-tools-music/ and https://cs224d.stanford.edu/reports/allenh.pdf for some previous work in music generation via deep learning.
+
+=========================
+
+Overview of methodology, simplifications, and tricks:
+
+- Midi2Symbolic.java parses MIDI files and outputs melodies in symbolic form.
+You can feed the melodies as input to a neural network that learns to compose melodies similar to the input melodies.
+- Given a Midi file, the program outputs one or more strings representing the melodies appearing in that midi file.
+- If you point Midi2Symbolic.java at a directory, it processes all midi files under that directory. All extracted melodies are appended to a single output file.
+- Each melody string is a sequence of characters representing either notes (e.g., middle C), rests, or durations. Each note is followed by a duration. Each rest is also followed by a duration.
+- There is a utility class, PlaySymbolic.java, that converts melody strings into MIDI and plays the melody on your computer's builtin synthesizer (tested on Windows).
+- To limit the number of possible symbols,  the program restricts pitches to a two-octave interval centered on Middle C. Notes outside that those two octaves are moved up or down an octave until they're inside the interval.
+- The program ignores gradations of volume (aka "velocity") of MIDI notes. It also ignores other effects such as pitch bending.
+- For each MIDI file that it processes, and for each MIDI track appearing in that file, Midi2Symbolic outputs zero or more melodies for that track.
+- The parser skips percussion tracks.
+- To exclude uninteresting melodies (e.g., bass accompaniments or melodies with long silences) the program skips melodies that have too many consecutive identical notes, too small a variety of pitches, too much silence, or too long rests between notes.
+- For polyphonic tracks, the program outputs (up to) two monophonic melodies from the track:  (i) the top notes of the harmony and (ii) the bottom notes of the harmony.
+- A monophonic track results in a single output melody.
+- The sequence of pitches in a melody is represented by a sequence of intervals (delta pitches). Effectively, all melodies are transposed to a standard key.
+- To handle different tempos, the program normalize all durations relative to the average duration of notes in the melody.
+- The program quanticizes tempos into 32 possible durations: 1*d, 2*d, 3*d, .... 32*d, where d is 1/8th the duration of the average note. Assuming the average note is a quarter note, the smallest possible duration for notes and rests is typically a 1/32nd note.
+- The longest duration for a note or a rest is four times the duration of the average note. Typically, this means that notes longer than a whole note are truncated to be a whole note in length.
+- No attempt is made to learn the characteristics of different instruments (e.g. pianos versus violins).
+- MelodyModelingExample.java  is the neural network code that composes melodies.
+- It's closely based on GravesLSTMCharModellingExample.java by Alex Black.
+- At the end of each learning epoch, MelodyModelingExample.java plays 15 seconds of the last melody generated. As learning progresses you can hear the compositions getting better.
+- Before exiting, MelodyModelingExample.java writes the generated melodies to a specified file (in reverse order, so that the highest quality melodies tend to be at the start of the file).
+- The melody strings composed by the neural network sometimes have invalid syntax (especially at the beginning of training). For example, in a valid melody string, each pitch character is followed by a duration character.  PlaySymbolic.java will ignore invalid characters  in melody strings.
+- http://truthsite.org/music/composed-in-the-style-of-bach.txt and http://truthsite.org/music/composed-in-the-style-of-pop.txt are sample melodies generated by the neural network.
+ Each line in the files is one melody. You can play the melodies with PlaySymbolic.java .
+- You can download some MIDI files from http://truthsite.org/music/bach-midi.zip and from http://musedata.org .
+- You can download symbolic music files from http://truthsite.org/music/midi-melodies-bach.txt.gz and http://truthsite.org/music/midi-melodies-pop.txt.gz .
+
+Possible directions of improvement
+
+ a. Represent durations numerically, not symbolically. Currently, both pitches and durations are represented symbolically (as characters), not numerically.  This makes sense for pitches probably, since the qualitative feel of a C followed by a G is quite different from the qualitative feel of a C followed by a G#. Likewise, following a C, a G is more similar to a E than to a G#; both E and G are notes in a C major chord.  But for tempos, a 32d note is more similar to a 16th note than to a quarter, etc.
+
+ b. Explore different activation functions, numbers of layers, and network hyper-parameters.
+
+ c. Download more MIDI files (e.g., from http://www.musedata.org) and run larger experiments.
+
+ d. Handle polyphonic music.
+
+ e. Handle gradations of volume, as well as other effects such as vibrato and pitch bend.
+
+ f. Learn different features for different instruments.
