504c1476e67a8d7a317cd8ccd2ca361479b803f8
Don A. Smith <ThinkerFeeler@gmail.com>
2017-04-01 16:08:45 -0700

    A listener for visualizing gradients in real time in 3D using JavaFX. GradientsListener extends TrainingListener and calls GradientsViewer to do the visualization. GradientsListener should work with any MultiLayerNetwork. GradientsListenerExample is a demo.

---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/userInterface/GradientsListenerExample.java
@@ -0,0 +1,32 @@
+package org.deeplearning4j.examples.userInterface;
+
+import org.deeplearning4j.examples.userInterface.util.GradientsListener;
+import org.deeplearning4j.examples.userInterface.util.UIExampleUtils;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+/**
+ * A simple example of how to view gradients in a running network using JavaFX 3d
+ *
+ * @author Donald A. Smith, Alex Black
+ */
+public class GradientsListenerExample {
+
+    public static void main(String[] args){
+
+        //Get our network and training data
+        MultiLayerNetwork net = UIExampleUtils.getMnistNetwork();
+        DataSetIterator trainData = UIExampleUtils.getMnistData();
+
+        System.out.println();
+        for(Layer layer:net.getLayers()) {
+            System.out.println(layer);
+        }
+        System.out.println();
+        net.setListeners(new GradientsListener(net,100));
+        net.fit(trainData);
+
+    }
+}
+
