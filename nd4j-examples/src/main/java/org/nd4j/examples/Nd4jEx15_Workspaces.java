9f2989ea7121439578b983ca564ab416e372282b
raver119 <raver119@gmail.com>
2017-04-10 13:48:21 +0300

    examples?

---
+++ nd4j-examples/src/main/java/org/nd4j/examples/Nd4jEx15_Workspaces.java
@@ -0,0 +1,9 @@
+package org.nd4j.examples;
+
+/**
+ * This example shows how to use memory Workspaces with ND4j for cyclic workloads
+ *
+ * @author raver119@gmail.com
+ */
+public class Nd4jEx15_Workspaces {
+}
