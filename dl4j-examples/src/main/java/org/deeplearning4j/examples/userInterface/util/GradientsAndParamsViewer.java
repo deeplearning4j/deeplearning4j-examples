bcf260b6b0a6a14933ab349005b4481cd8acc582
Don A. Smith <ThinkerFeeler@gmail.com>
2017-04-16 13:21:21 -0700

    Fix bug: during pausing and stepping, update radius of selected sphere.

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/userInterface/util/GradientsAndParamsViewer.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/userInterface/util/GradientsAndParamsViewer.java
@@ -185,7 +185,7 @@ public class GradientsAndParamsViewer extends Application {
                 shapesGroup.getChildren().add(this);
                 initialized = true;
             }
-            if (needsUpdate) {
+            if (needsUpdate || stepping || paused) {
                 //double hue=360.0/(1+ Math.exp(gradientFactor*lastGradient));
                 double hue = 260 - 260.0 / (1 + Math.exp(gradientFactor * lastGradient));
                 // Blue is negative, Green is near zero, Red is positive.
