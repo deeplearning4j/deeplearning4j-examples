39611b144f158247af3ca6484d539d8e26a5146a
Ganesh Krishnan <github@alphasigma.com.au>
2017-06-24 22:41:01 +1000

    Update BasicCSVClassifier.java
    changed misleading comments

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/dataexamples/BasicCSVClassifier.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/dataexamples/BasicCSVClassifier.java
@@ -54,8 +54,8 @@ public class BasicCSVClassifier {
         try {
 
             //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
-            int labelIndex = 4;     //5 values in each row of the iris.txt CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
-            int numClasses = 3;     //3 classes (types of iris flowers) in the iris data set. Classes have integer values 0, 1 or 2
+            int labelIndex = 4;     //5 values in each row of the animals.csv CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
+            int numClasses = 3;     //3 classes (types of animals) in the animals data set. Classes have integer values 0, 1 or 2
 
             int batchSizeTraining = 30;    //Iris data set: 150 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
             DataSet trainingData = readCSVDataset(
