303c3bdc9099dce1b92bd863d87c56930b88a39d
eraly <susan.eraly@gmail.com>
2017-07-20 20:31:05 -0700

    test with conv net

---
+++ dl4j-spark-examples/dl4j-spark/src/main/java/org/deeplearning4j/cnn/LenetMnistDistributedExample.java
@@ -0,0 +1,171 @@
+package org.deeplearning4j.cnn;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.LearningRatePolicy;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.spark.api.RDDTrainingApproach;
+import org.deeplearning4j.spark.api.TrainingMaster;
+import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
+import org.deeplearning4j.spark.parameterserver.training.SharedTrainingMaster;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.nd4j.parameterserver.distributed.conf.VoidConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by susaneraly on 7/20/17.
+ */
+public class LenetMnistDistributedExample {
+    private static final Logger log = LoggerFactory.getLogger(LenetMnistDistributedExample.class);
+
+    @Parameter(names = "-useSparkLocal", description = "Use spark local (helper for testing/running without spark submit)", arity = 1)
+    private boolean useSparkLocal = false;
+
+    @Parameter(names = "-batchSizePerWorker", description = "Number of examples to fit each worker with")
+    private int batchSizePerWorker = 64;
+
+    @Parameter(names = "-numEpochs", description = "Number of epochs for training")
+    private int numEpochs = 1;
+
+    public static void main(String[] args) throws Exception {
+        new LenetMnistDistributedExample().entryPoint(args);
+    }
+
+    protected void entryPoint(String[] args) throws Exception {
+        //Handle command line arguments
+        JCommander jcmdr = new JCommander(this);
+        try {
+            jcmdr.parse(args);
+        } catch (ParameterException e) {
+            //User provides invalid input -> print the usage info
+            jcmdr.usage();
+            try {
+                Thread.sleep(500);
+            } catch (Exception e2) {
+            }
+            throw e;
+        }
+
+        SparkConf sparkConf = new SparkConf();
+        if (useSparkLocal) {
+            sparkConf.setMaster("local[*]");
+        }
+        sparkConf.setAppName("DL4J Spark MLP Example");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        //Load the data into memory then parallelize
+        //This isn't a good approach in general - but is simple to use for this example
+        DataSetIterator iterTrain = new MnistDataSetIterator(batchSizePerWorker, true, 12345);
+        DataSetIterator iterTest = new MnistDataSetIterator(batchSizePerWorker, true, 12345);
+        List<DataSet> trainDataList = new ArrayList<>();
+        List<DataSet> testDataList = new ArrayList<>();
+        while (iterTrain.hasNext()) {
+            trainDataList.add(iterTrain.next());
+        }
+        while (iterTest.hasNext()) {
+            testDataList.add(iterTest.next());
+        }
+
+        JavaRDD<DataSet> trainData = sc.parallelize(trainDataList);
+        JavaRDD<DataSet> testData = sc.parallelize(testDataList);
+
+        // learning rate schedule in the form of <Iteration #, Learning Rate>
+        Map<Integer, Double> lrSchedule = new HashMap<>();
+        lrSchedule.put(0, 0.01);
+        lrSchedule.put(1000, 0.005);
+        lrSchedule.put(3000, 0.001);
+
+        int nChannels = 1;
+        int outputNum = 10;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(12345)
+            .regularization(true).l2(0.0005)
+            .learningRate(.01)//.biasLearningRate(0.02)
+            .learningRateDecayPolicy(LearningRatePolicy.Schedule)
+            .learningRateSchedule(lrSchedule)
+            .weightInit(WeightInit.XAVIER)
+            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+            .updater(Updater.NESTEROVS).momentum(0.9)
+            .list()
+            .layer(0, new ConvolutionLayer.Builder(5, 5)
+                .nIn(nChannels)
+                .stride(1, 1)
+                .nOut(20)
+                .activation(Activation.IDENTITY)
+                .build())
+            .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                .kernelSize(2,2)
+                .stride(2,2)
+                .build())
+            .layer(2, new ConvolutionLayer.Builder(5, 5)
+                .stride(1, 1)
+                .nOut(50)
+                .activation(Activation.IDENTITY)
+                .build())
+            .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                .kernelSize(2,2)
+                .stride(2,2)
+                .build())
+            .layer(4, new DenseLayer.Builder().activation(Activation.RELU)
+                .nOut(500).build())
+            .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .nOut(outputNum)
+                .activation(Activation.SOFTMAX)
+                .build())
+            .setInputType(InputType.convolutionalFlat(28,28,1)) //See note below
+            .backprop(true).pretrain(false).build();
+
+        VoidConfiguration voidConfiguration = VoidConfiguration.builder()
+            .unicastPort(40123)
+            .networkMask("10.1.1.0/24")
+            .controllerAddress(useSparkLocal ? "127.0.0.1" : null)
+            .build();
+
+        TrainingMaster tm = new SharedTrainingMaster.Builder(voidConfiguration, batchSizePerWorker)
+            .updatesThreshold(1e-3)
+            .rddTrainingApproach(RDDTrainingApproach.Direct)
+            .batchSizePerWorker(batchSizePerWorker)
+            .workersPerNode(4)
+            .build();
+
+        SparkDl4jMultiLayer sparkNet = new SparkDl4jMultiLayer(sc, conf, tm);
+
+        for (int i = 0; i < numEpochs; i++) {
+            sparkNet.fit(trainData);
+            log.info("Completed Epoch {}", i);
+        }
+
+        Evaluation evaluation = sparkNet.evaluate(testData);
+        log.info("***** Evaluation *****");
+        log.info(evaluation.stats());
+
+        tm.deleteTempFiles(sc);
+
+        log.info("***** Example Complete *****");
+    }
+}
