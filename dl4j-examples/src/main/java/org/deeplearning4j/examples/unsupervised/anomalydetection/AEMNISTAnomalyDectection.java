49b74dac75a0325a01c09184786257c2198e73aa
wangfeng-skymind <wangfeng@skymind.io>
2018-01-12 11:15:39 +0800

    Adding the unsupervised example for anormaly detection on the MNIST

---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/unsupervised/anomalydetection/AEMNISTAnomalyDectection.java
@@ -0,0 +1,155 @@
+package org.deeplearning4j.examples.unsupervised.anomalydetection;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+/**this's unsupervised examples for anormaly detection using autoencoder on MNIST,the example don't use the label of the MNIST
+ * when doing the example,the dataset contains  some digits writeen by me,especially those images don't look like digit
+ * of course you can use ImageWithBytesUtil  to generate images from ubyte format files of MNIST, and then add your pictures to training or testing directory .
+ * The goal is to identify outliers digits, i.e., those digits that are unusual or not like the typical digits.
+ * in fact, the normal images should have low reconfiguration error,whereas those weird pictures have high reconstruction error
+ *
+ * @author WANG FENG
+ */
+public class AEMNISTAnomalyDectection {
+    private static final Logger log = LoggerFactory.getLogger(AEMNISTAnomalyDectection.class);
+
+
+    private static String rootPath = System.getProperty("user.dir");
+    private static String modelPath = rootPath.substring(0, rootPath.lastIndexOf(File.separatorChar)) + File.separatorChar + "out" + File.separatorChar + "models" + File.separatorChar + "mnistAbnormalDetectedModel.json";
+
+    private static List<INDArray> featuresTrain = new ArrayList<>();
+    private static List<INDArray> featuresTest = new ArrayList<>();
+    private static double threshold = 0.01;//setting the value based on the situation when traning model shows loss value,
+
+
+    public static void main(String[] args) throws Exception {
+
+        MultiLayerNetwork net = createModel();
+        UIServer uiServer = UIServer.getInstance();
+        StatsStorage statsStorage = new InMemoryStatsStorage();
+        uiServer.attach(statsStorage);
+        net.setListeners(new StatsListener(statsStorage),new ScoreIterationListener(10));
+
+        DataSetIterator iter = new MnistIterator(100,true);
+        while(iter.hasNext()){
+            DataSet ds = iter.next();
+            featuresTrain.add(ds.getFeatureMatrix());
+        }
+        iter = new MnistIterator(100,false);
+        while(iter.hasNext()){
+            DataSet ds = iter.next();
+            featuresTest.add(ds.getFeatureMatrix());
+        }
+        //Train model
+        int nEpochs = 35;
+        for( int i = 0; i < nEpochs; i ++ ){
+            for(INDArray data : featuresTrain){
+                net.fit(data,data);
+            }
+            System.out.println("Epoch " + i + " complete");
+        }
+        List<Pair<Double,INDArray>> evalList = new ArrayList<>();
+        //the total num of the example is used for testing
+        int totalNum = iter.totalExamples();
+        double totalScore = 0;
+        for( int i = 0; i < featuresTest.size(); i ++ ){
+            INDArray testData = featuresTest.get(i);
+            int nRows = testData.rows();
+            for( int j = 0; j < nRows; j ++){
+                INDArray example = testData.getRow(j);
+                double score = net.score(new DataSet(example,example));
+                totalScore += score;
+                evalList.add(new ImmutablePair<>(score, example));
+            }
+            System.out.println("featuresTest " + i + " complete");
+        }
+        //Sort each list in the list by score
+        Comparator<Pair<Double, INDArray>> c = new Comparator<Pair<Double, INDArray>>() {
+            @Override
+            public int compare(Pair<Double, INDArray> o1, Pair<Double, INDArray> o2) {
+                return Double.compare(o1.getLeft(),o2.getLeft());
+            }
+        };
+
+        Collections.sort(evalList, c);
+        List<INDArray> normalList = new ArrayList<>();
+        List<INDArray> anomalyList = new ArrayList<>();
+
+        int listsize = evalList.size();
+        for( int j = 0; j < listsize && j < 60; j ++ ){
+            normalList.add(evalList.get(j).getRight());
+            anomalyList.add(evalList.get(listsize -j -1).getRight());
+
+        }
+        VisualizerUtil bestVisualizer = new VisualizerUtil(2.0,normalList,"best (High Rec. Error)");
+        bestVisualizer.visualize();
+        VisualizerUtil worstVisualizer = new VisualizerUtil(2.0,anomalyList,"Worst (High Rec. Error)");
+        worstVisualizer.visualize();
+
+        //save model
+        File modelFile = new File(modelPath);
+        if (!modelFile.exists()) {
+            modelFile.createNewFile();
+        }
+        log.info( modelFile.getPath() );
+
+        ModelSerializer.writeModel(net, modelFile,true);
+    }
+
+    public static MultiLayerNetwork createModel() {
+        //Set up network. 784 in/out (as MNIST images are 28x28).
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(123456)
+            .optimizationAlgo( OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+            .iterations(1)
+            .updater(Updater.ADAM)
+            .regularization(true)
+            .l2(1e-5)
+            .weightInit(WeightInit.XAVIER)
+            .learningRate(2e-3)
+            .activation(Activation.RELU)
+            .list()
+            .layer(0, new GravesLSTM.Builder().name("encoder0").nIn(784).nOut(800).build())
+            .layer(1, new GravesLSTM.Builder().name("encoder1").nIn(800).nOut(250).build())
+            .layer(2, new GravesLSTM.Builder().name("encoder2").nIn(250).nOut(10).build())
+            .layer(3, new GravesLSTM.Builder().name("decoder1").nIn(10).nOut(250).build())
+            .layer(4, new GravesLSTM.Builder().name("decoder2").nIn(250).nOut(800).build())
+            .layer(5, new RnnOutputLayer.Builder().name("output").nIn(800).nOut(784)
+                .activation(Activation.IDENTITY).lossFunction(LossFunctions.LossFunction.MSE).build())
+            .pretrain(false).backprop(true)
+            .build();
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+        return net;
+    }
+
+}
---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/unsupervised/anomalydetection/ImageWithByteUtil.java
@@ -0,0 +1,81 @@
+package org.deeplearning4j.examples.unsupervised.anomalydetection;
+
+import org.deeplearning4j.datasets.mnist.MnistManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.color.ColorSpace;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
+import java.io.File;
+
+/**the util will convert the ubyte into the image,this's ready for abnormal detected
+ * @author WANG FENG
+ */
+public class ImageWithByteUtil {
+    private static final Logger log = LoggerFactory.getLogger(ImageWithByteUtil.class);
+
+    protected static final String TEMP_ROOT = System.getProperty("user.home");
+    protected static final String MNIST_ROOT= TEMP_ROOT + File.separator + "MNIST" + File.separator;
+
+
+
+
+    public static void bytes2Image( ) throws Exception {
+        String images;
+        String labels;
+        int totalExamples;
+
+        images = MNIST_ROOT + "train-images-idx3-ubyte";
+        labels = MNIST_ROOT + "train-labels-idx1-ubyte";
+        totalExamples = 60000;
+        MnistManager man = new MnistManager(images, labels, true);
+        String path = File.separator + "home" + File.separator + "MNIST" + File.separator + "train" + File.separator;
+
+        BufferedImage bi = new BufferedImage(28,28,BufferedImage.TYPE_BYTE_GRAY);
+        for(int i = 0; i < totalExamples; ++ i) {
+            byte[] img = man.readImageUnsafe(i);
+            for( int k = 0; k < 784; k ++ ){
+                bi.setRGB(k % 28, k / 28,  img[k]);
+            }
+            log.info("the image is writing to the disk. training{}",i);
+            String filepath = path +  i + ".jpg";
+            ImageIO.write(bi, "jpg",new File(filepath));
+        }
+
+        images = MNIST_ROOT + "t10k-images-idx3-ubyte";
+        labels = MNIST_ROOT + "t10k-labels-idx1-ubyte";
+        totalExamples = 10000;
+        man = new MnistManager(images, labels, false);
+        path = File.separator + "home" + File.separator + "MNIST" + File.separator + "test" + File.separator;
+
+        for(int i = 0; i < totalExamples; ++ i) {
+            byte[] img = man.readImageUnsafe(i);
+            for( int k = 0; k < 784; k ++ ){
+                bi.setRGB(k % 28, k / 28,  img[k]);
+            }
+            log.info("the image is writing to the disk. testing{}",i);
+            String filepath = path +  i + ".jpg";
+            ImageIO.write(bi, "jpg",new File(filepath));
+        }
+
+    }
+    public static byte[] image2Bytes(File f) throws Exception {
+        BufferedImage bi = ImageIO.read(f);
+        int imageType = bi.getType();// 获取图片类型
+        int width = bi.getWidth();// 获取图片宽度
+        int height = bi.getHeight();// 获取图片高度
+
+        BufferedImage grayImage = new BufferedImage(width, height,BufferedImage.TYPE_BYTE_GRAY);
+        new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null).filter(bi, grayImage);
+        // getData方法返回BufferedImage的raster成员对象
+        return (byte[]) grayImage.getData().getDataElements(0, 0, width, height, null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        bytes2Image();
+    }
+
+
+}
