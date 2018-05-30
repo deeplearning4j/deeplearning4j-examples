6c29100c45bd0d0ad6888e6af08ebf723796b5c9
Don A. Smith <ThinkerFeeler@gmail.com>
2018-05-30 08:10:44 -0700

    Fix wrong numbers of instruments in comments

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/character/harmonies/PlayMusic.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/character/harmonies/PlayMusic.java
@@ -53,71 +53,71 @@ public class PlayMusic {
         "Celesta",                 //8
         "Glockenspiel",            //9
         "Music Box",               //10
-        "Vibraphone",              //12
-        "Marimba",                 //13
-        "Xylophone",               //14
-        "Tubular Bells",           //15
-        "Dulcimer",                //16
-        "Drawbar Organ",           //17
-        "Percussive Organ",        //18
-        "Rock Organ",              //19
-        "Church Organ",            //20
-        "Reed Organ",              //21
-        "Accordion",               //22
-        "Harmonica",               //23
-        "Tango Accordion",         //24
-        "Acoustic Guitar (nylon)", //25
-        "Acoustic Guitar (steel)", //26
-        "Electric Guitar (jazz)",  //27
-        "Electric Guitar (clean)", //28
-        "Electric Guitar (muted)", //29
-        "Overdriven Guitar",       //30
-        "Distortion Guitar",       //31
-        "Guitar Harmonics",        //32
-        "Acoustic Bass",           //33
-        "Electric Bass (finger)",  //34
-        "Electric Bass (pick)",    //35
-        "Fretless Bass",           //36
-        "Slap Bass 1",             //37
-        "Slap Bass 2",             //38
-        "Synth Bass 1",            //39
-        "Synth Bass 2",            //40
-        "Violin",                  //41
-        "Viola",                   //42
-        "Cello",                   //43
-        "Contrabass",              //44
-        "Tremolo Strings",         //45
-        "Pizzicato Strings",       //46
-        "Orchestral Harp",         //47
-        "Timpani",                 //48
-        "String Ensemble 1",       //49
-        "String Ensemble 2",       //50
-        "Synth Strings 1",         //51
-        "Synth Strings 2",         //52
-        "Choir Aahs",              //53
-        "Voice Oohs",              //54
-        "Synth Choir",             //55
-        "Orchestra Hit",           //56
-        "Trumpet",                 //57
-        "Trombone",                //58
-        "Tuba",                    //59
-        "Muted Trumpet",           //60
-        "French Horn",             //61
-        "Brass Section",           //62
-        "Synth Brass 1",           //63
-        "Synth Brass 2",           //64
-        "Soprano Sax",             //65
-        "Alto Sax",                //66
-        "Tenor Sax",               //67
-        "Baritone Sax",            //68
-        "Oboe",                    //69
-        "English Horn",            //70
-        "Bassoon",                 //71
+        "Vibraphone",              //11
+        "Marimba",                 //12
+        "Xylophone",               //13
+        "Tubular Bells",           //14
+        "Dulcimer",                //15
+        "Drawbar Organ",           //16
+        "Percussive Organ",        //17
+        "Rock Organ",              //18
+        "Church Organ",            //19
+        "Reed Organ",              //20
+        "Accordion",               //21
+        "Harmonica",               //22
+        "Tango Accordion",         //23
+        "Acoustic Guitar (nylon)", //24
+        "Acoustic Guitar (steel)", //25
+        "Electric Guitar (jazz)",  //26
+        "Electric Guitar (clean)", //27
+        "Electric Guitar (muted)", //28
+        "Overdriven Guitar",       //29
+        "Distortion Guitar",       //30
+        "Guitar Harmonics",        //31
+        "Acoustic Bass",           //32
+        "Electric Bass (finger)",  //33
+        "Electric Bass (pick)",    //34
+        "Fretless Bass",           //35
+        "Slap Bass 1",             //36
+        "Slap Bass 2",             //37
+        "Synth Bass 1",            //38
+        "Synth Bass 2",            //39
+        "Violin",                  //40
+        "Viola",                   //41
+        "Cello",                   //42
+        "Contrabass",              //43
+        "Tremolo Strings",         //44
+        "Pizzicato Strings",       //45
+        "Orchestral Harp",         //46
+        "Timpani",                 //47
+        "String Ensemble 1",       //48
+        "String Ensemble 2",       //49
+        "Synth Strings 1",         //50
+        "Synth Strings 2",         //51
+        "Choir Aahs",              //52
+        "Voice Oohs",              //53
+        "Synth Choir",             //54
+        "Orchestra Hit",           //55
+        "Trumpet",                 //56
+        "Trombone",                //57
+        "Tuba",                    //58
+        "Muted Trumpet",           //59
+        "French Horn",             //60
+        "Brass Section",           //61
+        "Synth Brass 1",           //62
+        "Synth Brass 2",           //63
+        "Soprano Sax",             //64
+        "Alto Sax",                //65
+        "Tenor Sax",               //66
+        "Baritone Sax",            //67
+        "Oboe",                    //68
+        "English Horn",            //69
+        "Bassoon",                 //70
         "Clarinet",                //71
         "Piccolo",                 //72
         "Flute",                   //73
         "Recorder",                //74
-        "Pan Flute",               //75 <-- max, For instruments higher, we treat them as guitar if we encode instruments.
+        "Pan Flute",               //75 <-- max, For instruments below, we treat them as guitar if we encode instruments.
         "Blown Bottle",            //76
         "Shakuhachi",              //77
         "Whistle",                 //78
