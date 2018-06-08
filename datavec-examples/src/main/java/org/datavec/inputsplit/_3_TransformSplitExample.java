545a3586bd94ee3e7b9145cbcae698886ee3dd89
ShamsUlAzeem <shamsazeem20@gmail.com>
2018-06-08 12:57:16 +0500

    NumberedFileInputSplit Examples

---
+++ datavec-examples/src/main/java/org/datavec/inputsplit/_3_TransformSplitExample.java
@@ -0,0 +1,7 @@
+package org.datavec.inputsplit;
+
+public class _3_TransformSplitExample {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ datavec-examples/src/main/java/org/datavec/inputsplit/_4_UsefulInputSplitOperations.java
@@ -0,0 +1,7 @@
+package org.datavec.inputsplit;
+
+public class _4_UsefulInputSplitOperations {
+    public static void main(String[] args) {
+
+    }
+}
