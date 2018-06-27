4c84520bd671970a9be8eeed10f88a25e9c665ca
ShamsUlAzeem <shamsazeem20@gmail.com>
2018-06-27 23:47:06 +0500

    LineRecordReader with multiple files

--- datavec-examples/src/main/java/org/datavec/recordreaders/Ex01_LineRecordReaderExample.java
+++ datavec-examples/src/main/java/org/datavec/recordreaders/Ex01_LineRecordReaderExample.java
@@ -6,6 +6,7 @@ import org.datavec.api.split.FileSplit;
 import org.datavec.api.split.InputStreamInputSplit;
 import org.nd4j.linalg.io.ClassPathResource;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.text.MessageFormat;
 
@@ -19,18 +20,18 @@ public class Ex01_LineRecordReaderExample {
           This example shows the working of a LineRecordReader with a single file. It will read the file
           line by line till it reaches the end.
          */
-        ClassPathResource classPathResource = new ClassPathResource("JoinExample/CustomerPurchases.csv");
-        FileSplit fileSplit = new FileSplit(
-            classPathResource.getFile()
+        ClassPathResource classPathResource1 = new ClassPathResource("JoinExample/CustomerPurchases.csv");
+        FileSplit fileSplit1 = new FileSplit(
+            classPathResource1.getFile()
         );
 
-        LineRecordReader lineRecordReader = new LineRecordReader();
-        lineRecordReader.initialize(fileSplit);
+        LineRecordReader lineRecordReader1 = new LineRecordReader();
+        lineRecordReader1.initialize(fileSplit1);
 
         System.out.println("--------------- Example 1: LineRecordReader with a single file ---------------");
         int i = 0;
-        while (lineRecordReader.hasNext()) {
-            System.out.println(MessageFormat.format("Line {0}: {1}", i++, lineRecordReader.next()));
+        while (lineRecordReader1.hasNext()) {
+            System.out.println(MessageFormat.format("Line {0}: {1}", i++, lineRecordReader1.next()));
         }
         System.out.println("------------------------------------------------------------\n\n\n");
 
@@ -42,15 +43,36 @@ public class Ex01_LineRecordReaderExample {
           You can also add listeners to your record readers to see what record are being read.
           Make sure you have log4j.properties setup in the resource folder prior to using LogRecordListener
          */
-        InputStreamInputSplit inputStreamInputSplit = new InputStreamInputSplit(new FileInputStream(classPathResource.getFile()));
-        LineRecordReader lineRecordReader1 = new LineRecordReader();
+        InputStreamInputSplit inputStreamInputSplit = new InputStreamInputSplit(new FileInputStream(classPathResource1.getFile()));
+        LineRecordReader lineRecordReader2 = new LineRecordReader();
         //
-        lineRecordReader1.setListeners(new LogRecordListener());
-        lineRecordReader1.initialize(inputStreamInputSplit);
+        lineRecordReader2.setListeners(new LogRecordListener());
+        lineRecordReader2.initialize(inputStreamInputSplit);
 
         System.out.println("--------------- Example 2: LineRecordReader with a listener ---------------");
-        while (lineRecordReader1.hasNext()) {
-            lineRecordReader1.next(); // This will put the logs on the console as a record is read
+        while (lineRecordReader2.hasNext()) {
+            lineRecordReader2.next(); // This will put the logs on the console as a record is read
+        }
+        System.out.println("------------------------------------------------------------\n\n\n");
+
+        //=====================================================================
+        //          Example 3: LineRecordReader with multiple files
+        //=====================================================================
+        /*
+          Every file in the FileSplit will be loaded one by one and will be read line by line
+         */
+        FileSplit fileSplit2 = new FileSplit(
+            new ClassPathResource("JoinExample").getFile().getParentFile(), // The parent "resources" folder path
+            new String[]{ "csv" }
+        );
+
+        LineRecordReader lineRecordReader3 = new LineRecordReader();
+        lineRecordReader3.initialize(fileSplit2);
+
+        System.out.println("--------------- Example 3: LineRecordReader with multiple files ---------------");
+        i = 0;
+        while (lineRecordReader3.hasNext()) {
+            System.out.println(MessageFormat.format("Line {0}: {1}", i++, lineRecordReader3.next()));
         }
         System.out.println("------------------------------------------------------------\n\n\n");
     }
