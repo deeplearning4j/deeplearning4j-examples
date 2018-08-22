ddd208f81ec89e50bdabb5c5c9be7b05a82e5c4f
Max Pumperla <max.pumperla@googlemail.com>
2018-08-22 12:50:31 +0200

    fix download

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/modelimport/keras/ImportDeepMoji.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/modelimport/keras/ImportDeepMoji.java
@@ -1,6 +1,7 @@
 package org.deeplearning4j.examples.modelimport.keras;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.KerasLayer;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
@@ -19,21 +20,33 @@ import static java.io.File.createTempFile;
  */
 public class ImportDeepMoji {
 
+    public static final String DATA_PATH = FilenameUtils.concat(System.getProperty("java.io.tmpdir"),
+        "dl4j_keras/");
+
+
     public static void main(String[] args) throws Exception {
+
+
         // First, register the Keras layer wrapped around our custom SameDiff attention layer
         KerasLayer.registerCustomLayer("AttentionWeightedAverage", KerasDeepMojiAttention.class);
 
         // Then, download the model from azure (check if it's cached)
+        //Create directory if required
+        File directory = new File(DATA_PATH);
+        if(!directory.exists()) directory.mkdir();
+
         String modelUrl = "http://blob.deeplearning4j.org/models/deepmoji.h5";
-        File cachedKerasFile = createTempFile("deepmoji", ".h5");
+        String downloadPath = DATA_PATH + "deepmoji_model.h5";
+        File cachedKerasFile = new File(downloadPath);
 
         if (!cachedKerasFile.exists()) {
             System.out.println("Downloading model to " + cachedKerasFile.toString());
             FileUtils.copyURLToFile(new URL(modelUrl), cachedKerasFile);
+            System.out.println("Download complete");
             cachedKerasFile.deleteOnExit();
         }
 
-        // Finally, import the model and test on artificial inputs.
+        // Finally, import the model and test on artificial input data.
         ComputationGraph graph = KerasModelImport.importKerasModelAndWeights(cachedKerasFile.getAbsolutePath());;
         INDArray input = Nd4j.create(new int[] {10, 30});
         graph.output(input);
