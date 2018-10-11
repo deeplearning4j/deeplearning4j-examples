32740a7ca5df211087e6f0ba8322a8a47293441f
raver119 <raver119@gmail.com>
2018-10-11 12:53:23 +0300

    random shapes

--- nd4j-examples/src/main/java/org/nd4j/examples/Random.java
+++ nd4j-examples/src/main/java/org/nd4j/examples/Random.java
@@ -1,5 +1,6 @@
 package org.nd4j.examples;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.nd4j.linalg.api.memory.MemoryWorkspace;
 import org.nd4j.linalg.api.memory.conf.WorkspaceConfiguration;
 import org.nd4j.linalg.api.memory.enums.AllocationPolicy;
@@ -10,7 +11,7 @@ import org.nd4j.linalg.factory.Nd4j;
 public class Random {
 
     public static void main(String[] args) throws Exception {
-        int[] shapes = new int[] {16, 32, 48, 64, 96, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
+        int[] shapes = new int[] {16, 32, 48, 64, 96, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768};
 
         WorkspaceConfiguration conf = WorkspaceConfiguration.builder()
             .initialSize(10L * 1024L * 1024L * 1024L)
@@ -35,6 +36,27 @@ public class Random {
 
                     Nd4j.getExecutioner().commit();
                 }
+            }
+        }
+
+
+        for (int e = 0; e < 100000; e++) {
+            int shapeR = RandomUtils.nextInt(8, 32768);
+            int shapeC = RandomUtils.nextInt(8, 32768);
+            long[] shape = new long[]{shapeR, shapeC};
+
+            try (MemoryWorkspace ws = Nd4j.getWorkspaceManager().getAndActivateWorkspace(conf, "id")) {
+                System.out.println("Trying N: {" + shapeR + ", " + shapeC + "}");
+                INDArray arrayN = Nd4j.randn(shape);
+
+                Nd4j.getExecutioner().commit();
+            }
+
+            try (MemoryWorkspace ws = Nd4j.getWorkspaceManager().getAndActivateWorkspace(conf, "id")) {
+                System.out.println("Trying U: {" + shapeR + ", " + shapeC + "}");
+                INDArray arrayU = Nd4j.rand(shape);
+
+                Nd4j.getExecutioner().commit();
             }
         }
     }
