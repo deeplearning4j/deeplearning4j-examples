f2cb9ca6b85810e6c1640fb6e2bbe5a7f7f1eea3
raver119 <raver119@gmail.com>
2018-10-11 12:46:13 +0300

    workspace

--- nd4j-examples/src/main/java/org/nd4j/examples/Random.java
+++ nd4j-examples/src/main/java/org/nd4j/examples/Random.java
@@ -1,5 +1,7 @@
 package org.nd4j.examples;
 
+import org.nd4j.linalg.api.memory.MemoryWorkspace;
+import org.nd4j.linalg.api.memory.conf.WorkspaceConfiguration;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
@@ -8,22 +10,25 @@ public class Random {
     public static void main(String[] args) throws Exception {
         int[] shapes = new int[] {16, 32, 48, 64, 96, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
 
+        WorkspaceConfiguration conf = WorkspaceConfiguration.builder()
+            .initialSize(10L * 1024L * 1024L * 1024L)
+            .build();
+
         for (int shapeR: shapes) {
             for (int shapeC: shapes) {
-                long[] shape = new long[] {shapeR, shapeC};
+                try (MemoryWorkspace ws = Nd4j.getWorkspaceManager().getAndActivateWorkspace(conf, "id")) {
+                    long[] shape = new long[]{shapeR, shapeC};
 
-                System.out.println("Trying N: {" + shapeR + ", " + shapeC + "}");
-                INDArray arrayN = Nd4j.randn(shape);
+                    System.out.println("Trying N: {" + shapeR + ", " + shapeC + "}");
+                    INDArray arrayN = Nd4j.randn(shape);
 
-                Nd4j.getExecutioner().commit();
+                    Nd4j.getExecutioner().commit();
 
-                System.out.println("Trying U: {" + shapeR + ", " + shapeC + "}");
-                INDArray arrayU = Nd4j.rand(shape);
+                    System.out.println("Trying U: {" + shapeR + ", " + shapeC + "}");
+                    INDArray arrayU = Nd4j.rand(shape);
 
-                Nd4j.getExecutioner().commit();
-                System.gc();
-                Thread.sleep(1000);
-                System.gc();
+                    Nd4j.getExecutioner().commit();
+                }
             }
         }
     }
