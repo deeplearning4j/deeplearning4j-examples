dc2ea6afc596ebd1a9abc242cc5d9169a6d1849a
Darek Zbyrad <dariusz.zbyrad@gmail.com>
2019-03-29 15:21:30 +0100

    Fixed typo

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/dataexamples/BasicCSVClassifier.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/dataexamples/BasicCSVClassifier.java
@@ -57,7 +57,7 @@ public class BasicCSVClassifier {
             int labelIndex = 4;     //5 values in each row of the animals.csv CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
             int numClasses = 3;     //3 classes (types of animals) in the animals data set. Classes have integer values 0, 1 or 2
 
-            int batchSizeTraining = 30;    //Animals training data set: 44 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
+            int batchSizeTraining = 30;    //Animals training data set: 30 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
             DataSet trainingData = readCSVDataset(
                     "/DataExamples/animals/animals_train.csv",
                     batchSizeTraining, labelIndex, numClasses);
