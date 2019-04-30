1d93cd6a210284b263d2adac79b9fbe9a2bc1ecf
AlexDBlack <blacka101@gmail.com>
2019-04-30 15:33:15 +1000

    Small fix

--- nd4j-examples/src/main/java/org/nd4j/samediff/Ex3_Variables.java
+++ nd4j-examples/src/main/java/org/nd4j/samediff/Ex3_Variables.java
@@ -72,7 +72,7 @@ public class Ex3_Variables {
         // on every forward pass
         SDVariable scalar = sd.scalar("scalar", 0.5);
         SDVariable zero = sd.zero("zero", new long[]{3,4});
-        SDVariable zeroToNine = sd.linspace("zeroToNine", 0, 9, 10);
+        SDVariable zeroToNine = sd.linspace("zeroToNine", DataType.FLOAT, 0, 9, 10);
         SDVariable randomUniform = sd.random().uniform(-1, 1, 3,4);      //-1 to 1, shape [3,4]
         SDVariable randomBernoulli = sd.random().bernoulli(0.5, 3,4);          //Random Bernoulli: 0 or 1 with probability 0.5
     }
