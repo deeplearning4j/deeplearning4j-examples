f415e674c3c5a629410414d8c9761ee2c3a396ba
Aleksandar Rogovic <rogovicaleks@gmail.com>
2019-05-05 12:25:59 +0200

    Add denseNet example (#798)

---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/denseNet/DenseNetModel.java
@@ -0,0 +1,32 @@
+package org.deeplearning4j.examples.denseNet;
+
+import org.deeplearning4j.nn.graph.ComputationGraph;
+
+public class DenseNetModel {
+
+    private static DenseNetModel instance;
+
+    public static DenseNetModel getInstance() {
+        if (instance == null) {
+            instance = new DenseNetModel();
+        }
+        return instance;
+    }
+
+    public ComputationGraph buildNetwork(long seed, int channels, int numLabels, int width, int height) {
+
+        DenseNetBuilder denseNetModel = new DenseNetBuilder(height, width, channels, seed, 12, false); //227x227x3
+
+        String init = denseNetModel.initLayer(7, 2, 1, channels); //56x56x24
+        String[] block1 = denseNetModel.addDenseBlock(6, true, "db1", new String[]{init});
+        String trans1 = denseNetModel.addTransitionLayer("tr1", 96, block1); //28x28x48
+        String[] block2 = denseNetModel.addDenseBlock(12, true, "db2", new String[]{trans1});
+        String trans2 = denseNetModel.addTransitionLayer("tr2", 192, block2); //14x14x96
+        String[] block3 = denseNetModel.addDenseBlock(24, true, "db3", new String[]{trans2});
+        String trans3 = denseNetModel.addTransitionLayer("tr3", 384, block3); //7x7x192
+        String[] block4 = denseNetModel.addDenseBlock(16, true, "db4", new String[]{trans3});
+        denseNetModel.addOutputLayer(7, 7, 384, numLabels, block4);
+
+        return denseNetModel.getModel();
+    }
+}
---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/denseNet/imageUtils/NoiseTransform.java
@@ -0,0 +1,51 @@
+package org.deeplearning4j.examples.denseNet.imageUtils;
+
+import org.bytedeco.javacpp.indexer.UByteIndexer;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.datavec.image.data.ImageWritable;
+import org.datavec.image.transform.BaseImageTransform;
+
+import java.util.Random;
+
+public class NoiseTransform extends BaseImageTransform<Mat> {
+
+    private int numPix;
+
+    public NoiseTransform(int numPix) {
+        this(null, numPix);
+    }
+
+    public NoiseTransform(Random random, int numPix) {
+        super(random);
+        this.numPix = numPix;
+        this.converter = new OpenCVFrameConverter.ToMat();
+    }
+
+    @Override
+    protected ImageWritable doTransform(ImageWritable image, Random random) {
+        if (image == null) {
+            return null;
+        }
+        Mat mat = converter.convert(image.getFrame());
+
+        int nbChannels = mat.channels();
+        UByteIndexer idx = mat.createIndexer();
+
+        for (int i = 0; i < numPix; i++) {
+            int row = random.nextInt(mat.rows());
+            int col = random.nextInt(mat.cols());
+            for (int j = 0; j < nbChannels; j++) {
+                //int current = idx.get(row, col);
+                //idx.put(row, col, j, random.nextInt(getMaxValue(current) - getMinValue(current) + 1) + getMinValue(current));
+                idx.put(row, col, j, random.nextInt(255));
+            }
+        }
+        return new ImageWritable(converter.convert(mat));
+    }
+
+    @Override
+    public float[] query(float... coordinates) {
+        return coordinates;
+    }
+}
