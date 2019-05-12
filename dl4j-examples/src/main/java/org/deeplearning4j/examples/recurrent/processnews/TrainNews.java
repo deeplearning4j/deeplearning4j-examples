a68a96887c87c72d387003bd2f021d99380ce9bc
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-05-12 22:53:07 +0300

    Update copyright headers

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/processnews/TrainNews.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/processnews/TrainNews.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2019 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
 /**-
  * This program trains a RNN to predict category of a news headlines. It uses word vector generated from PrepareWordVector.java.
  * - Labeled News are stored in \dl4j-examples\src\main\resources\NewsData\LabelledNews folder in train and test folders.
