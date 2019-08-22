5b5277ee69d020bf9f645f47050c80bfb0a53861
eraly <susan.eraly@gmail.com>
2019-08-22 11:16:15 -0700

    cleanup+javadoc

--- shared-utilities/src/main/java/org/deeplearning4j/examples/download/DownloaderUtility.java
+++ shared-utilities/src/main/java/org/deeplearning4j/examples/download/DownloaderUtility.java
@@ -7,38 +7,70 @@ import java.io.File;
 import java.net.URL;
 
 /**
+ * Given a base url and a zipped file name downloads contents to a specified directory under ~/dl4j-examples-data
+ * Will check md5 sum of downloaded file
+ * Assumption is zipped file contains a single directory with the same name as the zip file
+ *
+ * eg. With an instantiation DATAEXAMPLE(baseurl,"DataExamples.zip","data-dir",md5,size)
+ * DATAEXAMPLE.Download() will
+ *      download DataExamples.zip from baseurl/DataExamples.zip to a temp directory,
+ *      unzip it to ~/dl4j-example-data/data-dir
+ *      and return the unzipped path ~/dl4j-example-data/data-dir/DataExamples
+ *
  * @author susaneraly
  */
 public enum DownloaderUtility {
 
     /*
-        datavec-examples resources
+        Skymind datavec resources stored under AZURE_BLOB_URL/datavec-examples
      */
-    BASICDATAVECEXAMPLE("datavec-examples", "BasicDataVecExample.zip", "92f87e0ceb81093ff8b49e2b4e0a5a02", "1KB"),
-    INPUTSPLIT("datavec-examples", "inputsplit.zip", "f316b5274bab3b0f568eded9bee1c67f", "128KB"),
-    IRISDATA("datavec-examples","IrisData.zip","bb49e38bb91089634d7ef37ad8e430b8","1KB"),
-    JOINEXAMPLE("datavec-examples", "JoinExample.zip", "cbd6232cf1463d68ff24807d5dd8b530", "1KB"),
+    BASICDATAVECEXAMPLE("BasicDataVecExample.zip","datavec-examples", "92f87e0ceb81093ff8b49e2b4e0a5a02", "1KB"),
+    INPUTSPLIT("inputsplit.zip","datavec-examples", "f316b5274bab3b0f568eded9bee1c67f", "128KB"),
+    IRISDATA("IrisData.zip","datavec-examples","bb49e38bb91089634d7ef37ad8e430b8","1KB"),
+    JOINEXAMPLE("JoinExample.zip","datavec-examples", "cbd6232cf1463d68ff24807d5dd8b530", "1KB"),
     /*
-        dl4j-example resources
+        Skymind dl4j-examples resources stored under AZURE_BLOB_URL/dl4j-examples
      */
-    ANIMALS("dl4j-examples", "animals.zip", "1976a1f2b61191d2906e4f615246d63e", "820KB"),
-    ANOMALYSEQUENCEDATA("dl4j-examples", "anomalysequencedata.zip", "51bb7c50e265edec3a241a2d7cce0e73", "3MB"),
-    CAPTCHAIMAGE("dl4j-examples","captchaImage.zip","1d159c9587fdbb1cbfd66f0d62380e61","42MB"),
-    CLASSIFICATIONDATA("dl4j-examples","classification.zip","dba31e5838fe15993579edbf1c60c355","77KB"),
-    DATAEXAMPLES("dl4j-examples","DataExamples.zip","e4de9c6f19aaae21fed45bfe2a730cbb","2MB"),
-    LOTTERYDATA("dl4j-examples","lottery.zip","1e54ac1210e39c948aa55417efee193a","2MB"),
-    MODELIMPORT("dl4j-examples","modelimport.zip","411df05aace1c9ff587e430a662ce621","3MB"),
-    NEWSDATA("dl4j-examples","NewsData.zip","0d08e902faabe6b8bfe5ecdd78af9f64","21MB"),
-    NLPDATA("dl4j-examples","nlp.zip","1ac7cd7ca08f13402f0e3b83e20c0512","91MB"),
-    PREDICTGENDERDATA("dl4j-examples","PredictGender.zip","42a3fec42afa798217e0b8687667257e","3MB"),
-    STYLETRANSFER("dl4j-examples","styletransfer.zip","b2b90834d667679d7ee3dfb1f40abe94","3MB");
+    ANIMALS("animals.zip","dl4j-examples", "1976a1f2b61191d2906e4f615246d63e", "820KB"),
+    ANOMALYSEQUENCEDATA("anomalysequencedata.zip","dl4j-examples", "51bb7c50e265edec3a241a2d7cce0e73", "3MB"),
+    CAPTCHAIMAGE("captchaImage.zip","dl4j-examples","1d159c9587fdbb1cbfd66f0d62380e61","42MB"),
+    CLASSIFICATIONDATA("classification.zip","dl4j-examples","dba31e5838fe15993579edbf1c60c355","77KB"),
+    DATAEXAMPLES("DataExamples.zip","dl4j-examples","e4de9c6f19aaae21fed45bfe2a730cbb","2MB"),
+    LOTTERYDATA("lottery.zip","dl4j-examples","1e54ac1210e39c948aa55417efee193a","2MB"),
+    MODELIMPORT("modelimport.zip","dl4j-examples","411df05aace1c9ff587e430a662ce621","3MB"),
+    NEWSDATA("NewsData.zip","dl4j-examples","0d08e902faabe6b8bfe5ecdd78af9f64","21MB"),
+    NLPDATA("nlp.zip","dl4j-examples","1ac7cd7ca08f13402f0e3b83e20c0512","91MB"),
+    PREDICTGENDERDATA("PredictGender.zip","dl4j-examples","42a3fec42afa798217e0b8687667257e","3MB"),
+    STYLETRANSFER("styletransfer.zip","dl4j-examples","b2b90834d667679d7ee3dfb1f40abe94","3MB");
 
+    private final String BASE_URL;
     private final String DATA_FOLDER;
     private final String ZIP_FILE;
     private final String MD5;
     private final String DATA_SIZE;
+    private static final String AZURE_BLOB_URL = "https://deeplearning4jblob.blob.core.windows.net/dl4j-examples";
 
-    DownloaderUtility(String dataFolder, String zipFile, String md5, String dataSize) {
+    /**
+     * For use with resources uploaded to Azure blob storage.
+     * @param zipFile Name of zipfile. Should be a zip of a single directory with the same name
+     * @param dataFolder The folder to extract to under ~/dl4j-examples-data
+     * @param md5 of zipfile
+     * @param dataSize of zipfile
+     */
+    DownloaderUtility(String zipFile, String dataFolder, String md5, String dataSize) {
+       this(AZURE_BLOB_URL + "/" + dataFolder , zipFile, dataFolder, md5, dataSize);
+    }
+
+    /**
+     * Downloads a zip file from a base url to a specified directory under the user's home directory
+     * @param baseURL URL of file
+     * @param zipFile Name of zipfile to download from baseURL i.e baseURL+"/"+zipFile gives full URL
+     * @param dataFolder The folder to extract to under ~/dl4j-examples-data
+     * @param md5 of zipfile
+     * @param dataSize of zipfile
+     */
+    DownloaderUtility(String baseURL, String zipFile, String dataFolder, String md5, String dataSize) {
+        BASE_URL = baseURL;
         DATA_FOLDER = dataFolder;
         ZIP_FILE = zipFile;
         MD5 = md5;
@@ -46,7 +78,7 @@ public enum DownloaderUtility {
     }
 
     public String Download() throws Exception {
-        String dataURL = "https://deeplearning4jblob.blob.core.windows.net/dl4j-examples/" + DATA_FOLDER + "/" + ZIP_FILE;
+        String dataURL = BASE_URL + "/" + ZIP_FILE;
         String resourceName = ZIP_FILE.substring(0, ZIP_FILE.lastIndexOf(".zip"));
         String downloadPath = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), ZIP_FILE);
         String extractDir = FilenameUtils.concat(System.getProperty("user.home"), "dl4j-examples-data/" + DATA_FOLDER);
