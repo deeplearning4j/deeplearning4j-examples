9beabc24a3b51eff295d93da14c53ba7ece3f26a
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-08-25 04:19:18 +0300

    Add missing copyright headers for new source files (#878)

--- runexamples.sh
+++ runexamples.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+
+################################################################################
+# Copyright (c) 2015-2019 Skymind, Inc.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+################################################################################
+
 # Run one or all of the examples.
 # Use runexamples.sh -h to see the options.
 
@@ -116,7 +133,7 @@ then
     exit 1
   elif [ $which_one = 'q' ]; then
     exit 0
-  elif ! [[ $which_one =~ $re ]]; then 
+  elif ! [[ $which_one =~ $re ]]; then
     echo $error
     exit 1
   elif [ $which_one -le 0 -o $which_one -gt ${#arr[@]} ]; then
