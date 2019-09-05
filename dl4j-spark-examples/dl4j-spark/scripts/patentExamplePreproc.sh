7f6b00604d043f5e5319cc22592d3744841f23d1
skymindops <skymindops@skymind.io>
2019-09-05 12:24:50 +0300

    Update copyright headers to rest of the files

--- dl4j-spark-examples/dl4j-spark/scripts/patentExamplePreproc.sh
+++ dl4j-spark-examples/dl4j-spark/scripts/patentExamplePreproc.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+################################################################################
+# Copyright (c) 2015-2019 Skymind, Inc.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+################################################################################
+
 #REQUIRED arguments: Set these before running
 MASTER_IP=...                                   #IP address of master node
 AZURE_STORAGE_ACCT=...                          #Azure storage account name. 3-24 chars, lowercase alphanumeric only
