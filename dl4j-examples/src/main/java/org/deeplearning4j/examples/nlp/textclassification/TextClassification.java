b2f6510cc476664af9a6fc1af9ac9a9b9ba333bb
atuzhykov <andrewtuzhykov@gmail.com>
2020-02-25 18:38:50 +0000

    experiment10 (notes belong to commit name are here http://tiny.cc/yashkz)
    Signed-off-by: atuzhykov <andrewtuzhykov@gmail.com>

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/textclassification/TextClassification.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/textclassification/TextClassification.java
@@ -105,7 +105,7 @@ public class TextClassification {
         //Set up network configuration
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
             .seed(seed)
-            .updater(new Nadam(new SigmoidSchedule(ScheduleType.ITERATION,1e-2,0.5, 10)))
+            .updater(new Nadam(1e-1))
             .weightDecay(1e-6)
             .weightInit(WeightInit.XAVIER)
             .list()
