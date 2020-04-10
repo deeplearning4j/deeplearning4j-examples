ceb30da1ccb9ad24e95cfdcc9cc7f84b16b6e37b
Robert Altena <Rob@Ra-ai.com>
2020-04-10 21:10:50 +0900

    First commit
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

---
+++ android-examples/app/src/main/java/com/example/androidimageexperiment/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.androidimageexperiment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity instance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        instance = this;
+        setContentView(R.layout.activity_main);
+    }
+
+    public static MainActivity getInstance(){
+        return instance;
+    }
+}
