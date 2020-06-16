cb47cd5606255eef1d82d204236800f69b989dc2
Paul Dubs <paul.dubs@gmail.com>
2020-06-16 08:55:27 +0200

    Fix link to deepmoji model
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

--- tensorflow-keras-import-examples/src/main/java/org/deeplearning4j/modelimportexamples/keras/advanced/deepmoji/ImportDeepMoji.java
+++ tensorflow-keras-import-examples/src/main/java/org/deeplearning4j/modelimportexamples/keras/advanced/deepmoji/ImportDeepMoji.java
@@ -49,8 +49,7 @@ public class ImportDeepMoji {
         File directory = new File(DATA_PATH);
         if(!directory.exists()) directory.mkdir();
 
-        // TODO: fix link
-        String modelUrl = "http://blob.deeplearning4j.org/models/deepmoji.h5";
+        String modelUrl = "https://dl4jdata.blob.core.windows.net/models/deepmoji.h5";
         String downloadPath = DATA_PATH + "deepmoji_model.h5";
         File cachedKerasFile = new File(downloadPath);
 
